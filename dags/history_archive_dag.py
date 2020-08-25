14a4d383ede8b4da82f27865096a2559a6fd864f
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-25 19:47:38 -0400

    Added dags that export from archives and core

---
+++ dags/history_archive_dag.py
@@ -0,0 +1,40 @@
+import json
+
+from datetime import timedelta
+from subprocess import Popen
+
+from stellaretl_airflow.build_export_task import build_export_task
+from stellaretl_airflow.build_date_task import build_date_task
+
+from airflow import DAG, AirflowException
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+default_args = {
+    'owner': 'isaiahturner',
+    'depends_on_past': False,
+    'start_date': "2015-09-30T16:46:54+00:00",
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'history_archive_export',
+    default_args=default_args,
+    description='This DAG exports ledgers, transactions, and operations from the history archive to BigQuery.',
+    schedule_interval="*/5 * * * *",
+)
+
+date_task = build_date_task(dag)
+
+ledger_task = build_export_task(dag, 'archive', 'export_ledgers', 'ledgers.txt')
+
+tx_task = build_export_task(dag, 'archive', 'export_transactions', 'transactions.txt')
+
+op_task = build_export_task(dag, 'archive', 'export_operations', 'operations.txt')
+
+date_task >> ledger_task
+date_task >> tx_task
+date_task >> op_task
---
+++ dags/unbounded_core_dag.py
@@ -0,0 +1,35 @@
+import json
+
+from datetime import timedelta
+from subprocess import Popen
+from stellaretl_airflow.build_export_task import build_export_task
+from stellaretl_airflow.build_date_task import build_date_task
+
+from airflow import DAG, AirflowException
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+
+default_args = {
+    'owner': 'isaiahturner',
+    'depends_on_past': False,
+    'start_date': "2015-09-30T16:46:54+00:00",
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'unbounded_core_export',
+    default_args=default_args,
+    description='This DAG runs an unbounded stellar-core instance, which allows it to export accounts, offers, and trustlines to BigQuery. The core instance will \
+        continue running and exporting in the background.',
+    schedule_interval="*/5 * * * *",
+)
+
+date_task = build_date_task(dag)
+
+changes_task = build_export_task(dag, 'unbounded-core', 'export_ledger_entry_changes', 'changes.txt')
+
+date_task >> changes_task
