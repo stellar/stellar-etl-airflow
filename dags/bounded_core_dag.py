2ae2e1534687212e45efc719a11ff1173fa50dab
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-25 19:51:03 -0400

    Added dag for bounded core

---
+++ dags/bounded_core_dag.py
@@ -0,0 +1,34 @@
+import json
+
+from datetime import timedelta
+from subprocess import Popen
+
+from stellar_etl_airflow.build_export_task import build_export_task
+from stellar_etl_airflow.build_date_task import build_date_task
+
+from airflow import DAG, AirflowException
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+default_args = {
+    'owner': 'isaiahturner',
+    'depends_on_past': False,
+    'start_date': "2015-09-30T16:46:54+00:00",
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'bounded_core_export',
+    default_args=default_args,
+    description='This DAG runs a bounded stellar-core instance, which allows it to export accounts, offers, and trustlines to BigQuery.',
+    schedule_interval="*/5 * * * *",
+)
+
+date_task = build_date_task(dag)
+
+changes_task = build_export_task(dag, 'bounded-core', 'export_ledger_entry_changes', 'changes.txt')
+
+date_task >> changes_task
