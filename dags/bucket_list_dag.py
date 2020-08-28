368e543a1f775088d8ffae2ac486ff125b635a10
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-28 12:28:30 -0400

    Added load tasks

--- dags/bucket_list_dag.py
+++ dags/bucket_list_dag.py
@@ -3,6 +3,7 @@ from stellar_etl_airflow.build_date_task import build_date_task
 from stellar_etl_airflow.default import get_default_dag_args
 
 from airflow import DAG
+from airflow.models import Variable
 
 dag = DAG(
     'bucket_list_export',
@@ -11,13 +12,15 @@ dag = DAG(
     schedule_interval="*/5 * * * *",
 )
 
+file_names = Variable.get('output_file_names', deserialize_json=True)
+
 date_task = build_date_task(dag)
 
-acc_task = build_export_task(dag, 'bucket', 'export_accounts', 'accounts.txt')
+acc_task = build_export_task(dag, 'bucket', 'export_accounts', file_names['accounts'])
 
-off_task = build_export_task(dag, 'bucket', 'export_offers', 'offers.txt')
+off_task = build_export_task(dag, 'bucket', 'export_offers', file_names['offers'])
 
-trust_task = build_export_task(dag, 'bucket', 'export_trustlines', 'trustlines.txt')
+trust_task = build_export_task(dag, 'bucket', 'export_trustlines', file_names['trustlines'])
 
 date_task >> acc_task
 date_task >> off_task
