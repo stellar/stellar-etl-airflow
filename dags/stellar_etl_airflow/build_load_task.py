12ed0a9ee96313ecc68ee9c433bc0ac8b77e0bf5
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-28 12:28:20 -0400

    Fixed credentials

--- dags/stellar_etl_airflow/build_load_task.py
+++ dags/stellar_etl_airflow/build_load_task.py
@@ -1,13 +1,19 @@
-from airflow.operators.python_operator import PythonOperator
 from google.cloud import bigquery
-from airflow.models import Variable
-from airflow import AirflowException
+from google.oauth2 import service_account
 
-def append_to_bigquery(file_path, table_id):
-    client = bigquery.Client()
-    table_id = Variable.get('project_name') + Variable.get('database_name') + table_id
+from airflow import AirflowException
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+
+def append_to_bigquery(filename, table_id, **kwargs):
+    key_path = Variable.get('api_key_path')
+    credentials = service_account.Credentials.from_service_account_file(key_path, scopes=['https://www.googleapis.com/auth/bigquery'])
+
+    client = bigquery.Client(credentials=credentials, project=Variable.get('project_name'))
+    table_id = '.'.join([Variable.get('project_name'), Variable.get('database_name'), table_id])
     job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON, autodetect=True)   
 
+    file_path = Variable.get('output_path') + filename
     with open(file_path, "rb") as source_file:
         job = client.load_table_from_file(source_file, table_id, job_config=job_config)
     job.result()
@@ -16,11 +22,10 @@ def append_to_bigquery(file_path, table_id):
     if errors: 
         raise AirflowException("Loading command failed", errors) 
 
-def build_load_task(dag, exported_file, table_id):
+def build_load_task(dag, exported_filename, table_id):
     return PythonOperator(
             task_id='load_' + table_id + '_task',
             python_callable=append_to_bigquery,
-            op_kwargs={'file_path': exported_file, 'table_id': table_id},
-            provide_context=True,
+            op_kwargs={'filename': exported_filename, 'table_id': table_id},
             dag=dag,
         )
