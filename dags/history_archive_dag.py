4e35f41e91fc164e88744a3ba9bfc9d3d182f4e5
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-09-02 19:30:21 -0400

    Added gcs to bq tasks to history archive

--- dags/history_archive_dag.py
+++ dags/history_archive_dag.py
@@ -7,6 +7,7 @@ from stellar_etl_airflow.build_export_task import build_export_task
 from stellar_etl_airflow.build_time_task import build_time_task
 from stellar_etl_airflow.default import get_default_dag_args
 from stellar_etl_airflow.build_load_task import build_load_task
+from stellar_etl_airflow.build_gcs_to_bq_task import build_gcs_to_bq_task
 
 from airflow import DAG
 from airflow.models import Variable
@@ -19,25 +20,25 @@ dag = DAG(
 )
 
 file_names = Variable.get('output_file_names', deserialize_json=True)
-table_ids = Variable.get('table_ids', deserialize_json=True)
 
 date_task = build_time_task(dag)
 
 ledger_export_task = build_export_task(dag, 'archive', 'export_ledgers', file_names['ledgers'])
-
 tx_export_task = build_export_task(dag, 'archive', 'export_transactions', file_names['transactions'])
-
 op_export_task = build_export_task(dag, 'archive', 'export_operations', file_names['operations'])
-
-date_task >> ledger_export_task
-date_task >> tx_export_task
-date_task >> op_export_task
+trade_export_task = build_export_task(dag, 'archive', 'export_trades', file_names['trades'])
 
 load_ledger_task = build_load_task(dag, 'ledgers')
-ledger_export_task >> load_ledger_task
-
 load_tx_task = build_load_task(dag, 'transactions')
-tx_export_task >> load_tx_task
-
 load_op_task = build_load_task(dag, 'operations')
-op_export_task >> load_op_task
+load_trade_task = build_load_task(dag, 'trades')
+
+send_ledgers_to_bq_task = build_gcs_to_bq_task(dag, 'ledgers')
+send_txs_to_bq_task = build_gcs_to_bq_task(dag, 'transactions')
+send_ops_to_bq_task = build_gcs_to_bq_task(dag, 'operations')
+send_trades_to_bq_task = build_gcs_to_bq_task(dag, 'trades')
+
+date_task >> ledger_export_task >> load_ledger_task >> send_ledgers_to_bq_task
+date_task >> tx_export_task >> load_tx_task  >> send_txs_to_bq_task
+date_task >> op_export_task >> load_op_task  >> send_ops_to_bq_task
+date_task >> trade_export_task >> load_trade_task  >> send_trades_to_bq_task
