1d46225498d17390c7bbc4d56f889b6e826df0b0
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-09-03 16:21:30 -0400

    Used filesensor with wildcard support

--- dags/stellar_etl_airflow/build_file_sensor_task.py
+++ dags/stellar_etl_airflow/build_file_sensor_task.py
@@ -1,15 +1,15 @@
 import os
-from airflow.contrib.sensors.file_sensor import FileSensor
+from stellar_etl_airflow.glob_file_operator import GlobFileSensor
 from airflow.models import Variable
 
 def build_file_sensor_task(dag, data_type):
     file_names = Variable.get('output_file_names', deserialize_json=True)
     folder_name = file_names['changes']
-    base_filename = file_names[data_type]
-    return FileSensor(
+    filename = f'*{file_names[data_type]}' #include wildcard, as ledger ranges vary
+    return GlobFileSensor(
         task_id=f'{data_type}_file_sensor',
-        filepath= os.path.join(folder_name, base_filename),
-        poke_interval=300,
-        mode="reschedule",
+        filepath=os.path.join(Variable.get('output_path'), folder_name, filename),
+        poke_interval=60,
+        #mode="reschedule",
         dag=dag
     )
