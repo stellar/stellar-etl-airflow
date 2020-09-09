de34f2c0d3cc0e0d5913187754ca6cf392514858
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-09-09 14:03:00 -0400

    Improved documentation and variable names

--- dags/stellar_etl_airflow/merge_table_task.py
+++ dags/stellar_etl_airflow/merge_table_task.py
@@ -1,3 +1,7 @@
+'''
+This file contains functions for creating Airflow tasks to merge data on ledger entry changes from
+a file in Google Cloud storage into a BigQuery table.
+'''
 import json
 
 from airflow.models import Variable
@@ -11,13 +15,33 @@ from google.oauth2 import service_account
 
 
 def read_gcs_schema(data_type):
+    '''
+    Reads a the schema file corresponding to data_type from Google Cloud Storage and parses it.
+    Data types should be: 'accounts', 'ledgers', 'offers', 'operations', 'trades', 'transactions', or 'trustlines'.
+
+    Parameters:
+        data_type - type of the data being uploaded; should be string 
+    Returns:
+        the parsed schema
+    '''
+
     gcs_hook = GoogleCloudStorageHook(google_cloud_storage_conn_id='google_cloud_platform_connection')
     schema_fields = json.loads(gcs_hook.download(
         Variable.get('gcs_bucket_name'),
         f'schemas/{data_type}_schema.json').decode("utf-8"))
     return schema_fields
 
-def generate_strings_from_schema(schema, source_table_alias='S'):
+def generate_queries_from_schema(schema, source_table_alias):
+    '''
+    Generates the SQL queries that will insert or update a row in a table based on the provided schema.
+    
+    Parameters:
+        schema - an array of dictionaries, each containing the name, type, and description of a schema field 
+        source_table_alias - the name of the table being used as a data source
+    Returns:
+        the insert query and update query
+    '''
+
     insert = []
     update = []
     for field in schema:
@@ -29,33 +53,56 @@ def generate_strings_from_schema(schema, source_table_alias='S'):
     return f'INSERT ({insert_list}) VALUES ({insert_list})', f'UPDATE SET {updated_list}'
 
 def create_merge_query(temp_table_id, data_type, schema_fields):
+    '''
+    Creates the string representation of the merge query. Data types should be: 'accounts', 'offers', or 'trustlines'. 
+    
+    Parameters:
+        temp_table_id - the id of the temporary table where the external data is located 
+        data_type - type of the data being uploaded; should be string
+        schema_fields - the schema fields for the external data
+    Returns:
+        the newly created task
+    '''
+
     dataset_name = Variable.get('bq_dataset')
     true_table_id = Variable.get('table_ids')[data_type]
-
-    equality_field = ''
+    dest_alias = 'T'
+    source_alias = 'S'
+    
+    equality_comparison = ''
     if data_type == 'accounts':
-        equality_field = 'account_id'
+        equality_comparison = f'{dest_alias}.account_id = {source_alias}.account_id'
     elif data_type == 'offers':
-        equality_field = 'offer_id'
+        equality_comparison = f'{dest_alias}.offer_id = {source_alias}.offer_id'
     elif data_type == 'trustlines':
-        equality_field = ''
+        equality_comparison = f'{dest_alias}.account_id = {source_alias}.account_id AND {dest_alias}.asset_type = {source_alias}.asset_type AND {dest_alias}.asset_issuer = {source_alias}.asset_issuer AND {dest_alias}.asset_code = {source_alias}.asset_code'
     else:
-        raise AirflowException("Unable to write query: unknown data type ", data_type)
-    insert_string, update_string = generate_strings_from_schema(schema_fields)
-    query = f'''MERGE {dataset_name}.{true_table_id} T
-                USING {dataset_name}.{temp_table_id} S
-                ON T.{equality_field} = S.{equality_field}
-                WHEN MATCHED AND S.deleted THEN
+        raise AirflowException("Unable to write query: unknown data type for merges ", data_type)
+
+    insert_query, update_query = generate_queries_from_schema(schema_fields, source_alias)
+
+    query = f'''MERGE {dataset_name}.{true_table_id} {dest_alias}
+                USING {dataset_name}.{temp_table_id} {source_alias}
+                ON {equality_comparison}
+                WHEN MATCHED AND {source_alias}.deleted THEN
                     DELETE
                 WHEN MATCHED THEN
-                    {update_string}
+                    {update_query}
                 WHEN NOT MATCHED THEN
-                    {insert_string}'''
+                    {insert_query}'''
+
     return query
 
 def apply_gcs_changes(data_type, **kwargs):
     '''
-    https://cloud.google.com/bigquery/external-data-cloud-storage#temporary-tables
+    Sets up a file in Google Cloud Storage as an temporary table, and merges it with an existing table in BigQuery.
+    The file's location in GCS is retrieved through XCOM, and the schema for the temporary table is loaded from GCS.
+    Data types should be: 'accounts', 'offers', or 'trustlines'.
+
+    Parameters:
+        data_type - type of the data being uploaded; should be string 
+    Returns:
+        N/A
     '''
 
     key_path = Variable.get('api_key_path')
@@ -72,11 +119,7 @@ def apply_gcs_changes(data_type, **kwargs):
 
     sql = create_merge_query(table_id, data_type, schema)
 
-    # Make an API request.
     query_job = client.query(sql, job_config=job_config)
-
-    print(query_job.total_bytes_billed)
-
 
 def build_apply_gcs_changes_to_bq_task(dag, data_type):
     return PythonOperator(
