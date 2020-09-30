1239ade7eb59ba6539bfd2321f758dbc3ed7c349
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-09-30 16:11:25 -0400

    Added include change folder option for file sensors

--- dags/stellar_etl_airflow/build_file_sensor_task.py
+++ dags/stellar_etl_airflow/build_file_sensor_task.py
@@ -5,13 +5,14 @@ import os
 from stellar_etl_airflow.glob_file_operator import GlobFileSensor
 from airflow.models import Variable
 
-def build_file_sensor_task(dag, data_type):
+def build_file_sensor_task(dag, data_type, include_folder_in_path=False):
     file_names = Variable.get('output_file_names', deserialize_json=True)
     folder_name = file_names['changes']
     filename = f'*{file_names[data_type]}' #include wildcard, as ledger ranges vary
+    filepath = os.path.join(Variable.get('output_path'), folder_name, filename) if include_folder_in_path else os.path.join(Variable.get('output_path'), filename) 
     return GlobFileSensor(
         task_id=f'{data_type}_file_sensor',
-        filepath=os.path.join(Variable.get('output_path'), folder_name, filename),
+        filepath=filepath,
         base_path=Variable.get('output_path'),
         poke_interval=60,
         dag=dag
