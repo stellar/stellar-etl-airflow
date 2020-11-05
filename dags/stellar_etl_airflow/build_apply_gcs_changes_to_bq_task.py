9152137a9cc3a9fbac600c1d1da96a87a2b64b0e
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-11-05 16:58:08 -0500

    Added query type for orderbook tables

--- dags/stellar_etl_airflow/build_apply_gcs_changes_to_bq_task.py
+++ dags/stellar_etl_airflow/build_apply_gcs_changes_to_bq_task.py
@@ -12,6 +12,7 @@ from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 
 from google.cloud import bigquery
 from google.oauth2 import service_account
+from google.cloud.exceptions import NotFound
 
 from os.path import splitext, basename
 
@@ -20,17 +21,18 @@ from os.path import splitext, basename
 def read_local_schema(data_type):
     '''
     Reads the schema file corresponding to data_type and parses it.
-    Data types should be: 'accounts', 'ledgers', 'offers', 'operations', 'trades', 'transactions', or 'trustlines'.
+    Data types should be: 'accounts', 'ledgers', 'offers', 'operations', 'trades', 'transactions', 'trustlines',
+    'dimAccounts', 'dimOffers', 'dimMarkets', or 'factEvents'.
 
     Parameters:
         data_type - type of the data being uploaded; should be string
     Returns:
-        the parsed schema
+        the parsed schema as a dictionary
     '''
 
     # since the dags folder is shared among airflow workers and the webservers, schemas are stored in the dags folder
-    schema_filepath = os.path.join('/home/airflow/gcs/dags', f'schemas/{data_type}_schema.json') 
-    
+    #schema_filepath = os.path.join('/home/airflow/gcs/dags', f'schemas/{data_type}_schema.json') 
+    schema_filepath = f'/Users/isaiahturner/Local/schemas/{data_type}_schema.json'
     logging.info(f'Loading schema file at {schema_filepath}')
  
     with open(schema_filepath, 'r') as schema_file:
@@ -69,7 +71,7 @@ def generate_update_query(schema, source_table_alias):
 def generate_equality_comparison(data_type, source_table_alias, dest_table_alias):
     '''
     Generates the equality comparison used to determine if two rows are the same.
-    Data types should be: 'accounts', 'offers', or 'trustlines'. 
+    Data types should be: 'accounts', 'offers', 'trustlines', 'dimAccounts', 'dimOffers', or 'dimMarkets'. 
     Parameters:
         data_type - type of the data being uploaded; should be string
         source_table_alias - the name of the table being used as a data source
@@ -84,6 +86,9 @@ def generate_equality_comparison(data_type, source_table_alias, dest_table_alias
         'offers': f'{dest_table_alias}.offer_id = {source_table_alias}.offer_id',
         'trustlines': f'{dest_table_alias}.account_id = {source_table_alias}.account_id AND {dest_table_alias}.asset_type = {source_table_alias}.asset_type \
         AND {dest_table_alias}.asset_issuer = {source_table_alias}.asset_issuer AND {dest_table_alias}.asset_code = {source_table_alias}.asset_code',
+        'dimAccounts': f'{dest_table_alias}.account_id = {source_table_alias}.account_id',
+        'dimOffers': f'{dest_table_alias}.dim_offer_id = {source_table_alias}.dim_offer_id',
+        'dimMarkets': f'{dest_table_alias}.market_id = {source_table_alias}.market_id'
     }
 
     equality_comparison = switch.get(data_type, 'No comparison')
@@ -124,11 +129,39 @@ def create_merge_query(temp_table_id, data_type, schema_fields):
 
     return query
 
+def create_insert_unique_query(temp_table_id, data_type, schema_fields):
+    '''
+    Creates the string representation of the insert unique query. Data types should be: 'dimAccounts', 'dimOffers', or 'dimMarkets'. 
+    
+    Parameters:
+        temp_table_id - the id of the temporary table where the external data is located 
+        data_type - type of the data being uploaded; should be string
+        schema_fields - the schema fields for the external data
+    Returns:
+        the newly created task
+    '''
+    project_name = Variable.get('bq_project')
+    dataset_name = Variable.get('bq_dataset')
+    true_table_id = Variable.get('table_ids', deserialize_json=True)[data_type]
+    dest_alias = 'T'
+    source_alias = 'S'   
+
+    insert_query = generate_insert_query(schema_fields, source_alias)
+    equality_comparison = generate_equality_comparison(data_type, source_alias, dest_alias)
+
+    query = f'''MERGE `{project_name}.{dataset_name}.{true_table_id}` {dest_alias}
+                USING `{temp_table_id}` {source_alias}
+                ON {equality_comparison}
+                WHEN NOT MATCHED THEN
+                    {insert_query}'''
+
+    return query
+
 def apply_gcs_changes(data_type, **kwargs):
     '''
     Sets up a file in Google Cloud Storage as an temporary table, and merges it with an existing table in BigQuery.
     The file's location in GCS is retrieved through XCOM, and the schema for the temporary table is loaded from GCS.
-    Data types should be: 'accounts', 'offers', or 'trustlines'.
+    Data types should be: 'accounts', 'offers', 'trustlines', 'dimAccounts', 'dimOffers', or 'dimMarkets'.
 
     Parameters:
         data_type - type of the data being uploaded; should be string
@@ -142,11 +175,12 @@ def apply_gcs_changes(data_type, **kwargs):
 
     gcs_bucket_name = Variable.get('gcs_exported_data_bucket_name')
     gcs_filepath = kwargs['task_instance'].xcom_pull(task_ids=f'load_{data_type}_to_gcs')
-    schema = read_local_schema(data_type)
+    schema_dict = read_local_schema(data_type)
+    bq_schema_list = [bigquery.SchemaField(field['name'], field['type'], mode=field['mode']) for field in schema_dict] 
 
     external_config = bigquery.ExternalConfig('NEWLINE_DELIMITED_JSON')
     external_config.source_uris = [f'gs://{gcs_bucket_name}/{gcs_filepath}']
-    external_config.schema = [bigquery.SchemaField(field['name'], field['type'], mode=field['mode']) for field in schema]
+    external_config.schema = bq_schema_list
 
     # The temporary table is is equal to the name of the file used to create it. Table ids cannot have '-'. Instead they have '_'
     table_id = f'{splitext(basename(gcs_filepath))[0]}'
@@ -154,10 +188,26 @@ def apply_gcs_changes(data_type, **kwargs):
 
     job_config = bigquery.QueryJobConfig(table_definitions={table_id: external_config})
 
-    sql_query = create_merge_query(table_id, data_type, schema)
-    logging.info(f'Merge query is: {sql_query}')
-    logging.info(f'Running BigQuery job with config: {job_config._properties}')
+    #check if the table already exists; if it does not then we need to create it using the schema that we have already read in
+    true_table_id = f'{Variable.get("bq_project")}.{Variable.get("bq_dataset")}.{Variable.get("table_ids", deserialize_json=True)[data_type]}'
+    try:
+        _ = client.get_table(true_table_id)
+    except NotFound:
+        logging.info(f'Bigquery table not found; creating table with id {true_table_id}')
+        table = bigquery.Table(true_table_id, schema=bq_schema_list)
+        client.create_table(table)
 
+    if data_type in ['accounts', 'offers', 'trustlines']:
+        logging.info('Using merge query...')
+        sql_query = create_merge_query(table_id, data_type, schema_dict)
+    elif data_type in ['dimAccounts', 'dimOffers', 'dimMarkets']:
+        logging.info('Using insert unique query...')
+        sql_query = create_insert_unique_query(table_id, data_type, schema_dict)
+    else:
+        raise AirflowException(f'Data type {data_type} has no corresponding query')
+
+    logging.info(f'Query is: {sql_query}')
+    logging.info(f'Running BigQuery job with config: {job_config._properties}')
     query_job = client.query(sql_query, job_config=job_config)
     result_rows = query_job.result()
 
@@ -172,7 +222,7 @@ def apply_gcs_changes(data_type, **kwargs):
 def build_apply_gcs_changes_to_bq_task(dag, data_type):
     '''
     Creates a task that applies changes from a Google Cloud Storage file to a BigQuery table.
-    Data types should be: accounts, ledgers, offers, operations, trades, transactions, or trustlines.
+    Data types should be: 'accounts', 'offers', 'trustlines', 'dimAccounts', 'dimOffers', or 'dimMarkets'.
     
     Parameters:
         dag - the parent dag
