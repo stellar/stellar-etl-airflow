306491053035e07d38463b368f2e99449035c64d
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-11-05 16:59:15 -0500

    Added support for orderbooks to file sensor

--- dags/stellar_etl_airflow/build_file_sensor_task.py
+++ dags/stellar_etl_airflow/build_file_sensor_task.py
@@ -5,11 +5,30 @@ import os
 from stellar_etl_airflow.glob_file_operator import GlobFileSensor
 from airflow.models import Variable
 
-def build_file_sensor_task(dag, data_type, include_folder_in_path=False):
+def build_file_sensor_task(dag, data_type, is_change=False, is_orderbook=False):
+    '''
+    Creates a task that senses for local files.
+    Data types should be: 'accounts', 'ledgers', 'offers', 'operations', 'trades', 'transactions', 'trustlines',
+    'dimAccounts', 'dimOffers', 'dimMarkets', or 'factEvents'.
+    
+    Parameters:
+        dag - the parent dag
+        data_type - type of the data being uploaded; should be string
+        is_change - True if the file sensor should look in the changes folder
+        is_orderbook - True if the file sensor should look in the orderbook folder
+    Returns:
+        the newly created task
+    '''
     file_names = Variable.get('output_file_names', deserialize_json=True)
-    folder_name = file_names['changes']
+    change_folder_name = file_names['changes']
+    orderbook_folder_name = file_names['orderbooks']
     filename = f'*{file_names[data_type]}' #include wildcard, as ledger ranges vary
-    filepath = os.path.join(Variable.get('output_path'), folder_name, filename) if include_folder_in_path else os.path.join(Variable.get('output_path'), filename) 
+    filepath = filename
+    if is_change:
+        filepath = os.path.join(change_folder_name, filename)
+    elif is_orderbook:
+        filepath = os.path.join(orderbook_folder_name, filename)
+    filepath = os.path.join(Variable.get('output_path'), filepath)
     return GlobFileSensor(
         task_id=f'{data_type}_file_sensor',
         filepath=filepath,
