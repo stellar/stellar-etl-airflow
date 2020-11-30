fb91d2a621eacffd2acffc076c2de61e80b0da13
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-11-30 17:46:45 -0500

    Switch to KubernetesPodOperator (#47)
    * Converted time and export task to kubernetes pod operators
    
    * Removed unused DockerOperator code and imports
    
    * Added new variables
    
    * Added support for customized affinities and persistent volumes
    
    * Added variable for pod config location
    
    * Fix JSON formatting

--- dags/stellar_etl_airflow/build_time_task.py
+++ dags/stellar_etl_airflow/build_time_task.py
@@ -2,7 +2,7 @@
 This file contains functions for creating Airflow tasks to convert from a time range to a ledger range.
 '''
 
-from stellar_etl_airflow.docker_operator import DockerOperator  
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator 
 from airflow.models import Variable
 
 def build_time_task(dag, use_next_exec_time=True):
@@ -20,13 +20,21 @@ def build_time_task(dag, use_next_exec_time=True):
     '''
 
     end_time = '{{ next_execution_date.isoformat() }}' if use_next_exec_time else '{{ ts }}'
-    command = "stellar-etl get_ledger_range_from_times -s {{ ts }} --stdout -e " + end_time
-    return DockerOperator(
-        task_id='get_ledger_range_from_times',
-        image=Variable.get('image_name'),
-        command=command, 
-        volumes=[f'{Variable.get("local_output_path")}:{Variable.get("image_output_path")}'],
-        dag=dag,
-        xcom_push=True,
-        auto_remove=True,
-    )
+    command = ["stellar-etl"]
+    args = [ "get_ledger_range_from_times", "-s", "{{ ts }}", "-o", "/airflow/xcom/return.json", '-e', end_time]
+    config_file_location = Variable.get('kube_config_location')
+    in_cluster = False if config_file_location else True
+    return KubernetesPodOperator(
+         task_id='get_ledger_range_from_times',
+         name='get_ledger_range_from_times',
+         namespace='etl-tasks',
+         image=Variable.get('image_name'),
+         cmds=command,
+         arguments=args,
+         dag=dag,
+         do_xcom_push=True,
+         is_delete_operator_pod=True,
+         in_cluster=in_cluster,
+         config_file=config_file_location,
+         affinity=Variable.get('affinity', deserialize_json=True)
+     ) 
