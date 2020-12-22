9811c5052f1100c65b73383e2e2cd69f3aa34faa
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-12-22 14:20:10 -0500

    Add image_pull_policy and resources variables (#64)
    * Added resources and image_pull_policy variables
    
    * Added default values for new variables
    
    * Updated default vars

--- dags/stellar_etl_airflow/build_export_task.py
+++ dags/stellar_etl_airflow/build_export_task.py
@@ -115,7 +115,9 @@ def build_kubernetes_pod_exporter(dag, command, etl_cmd_string, output_file):
         config_file=config_file_location,
         volume_mounts=[data_mount],
         volumes=[data_volume],
-        affinity=Variable.get('affinity', deserialize_json=True)
+        affinity=Variable.get('affinity', deserialize_json=True),
+        resources=Variable.get('resources', default_var=None, deserialize_json=True),
+        image_pull_policy=Variable.get('image_pull_policy')
     )
 
 def build_docker_exporter(dag, command, etl_cmd_string, output_file):
@@ -132,6 +134,7 @@ def build_docker_exporter(dag, command, etl_cmd_string, output_file):
     from stellar_etl_airflow.docker_operator import DockerOperator 
 
     full_cmd = f'bash -c "{etl_cmd_string} && echo \"{output_file}\""'
+    force_pull = True if Variable.get('image_pull_policy')=='Always' else False
     return DockerOperator(
         task_id=command + '_task',
         image=Variable.get('image_name'),
@@ -140,6 +143,7 @@ def build_docker_exporter(dag, command, etl_cmd_string, output_file):
         dag=dag,
         xcom_push=True,
         auto_remove=True,
+        force_pull=force_pull,
     ) 
 
 def build_export_task(dag, cmd_type, command, filename):
