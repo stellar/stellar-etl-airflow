f1b4b05d3f30eb1c8e8c2de6ebbb45fc2a65f299
Alec Charbonneau <aleccharb21@gmail.com>
2021-04-23 21:11:54 -0700

    still add if empty, but sleep

--- poststart.sh
+++ poststart.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 move_file_if_closed() {
-    fileSize=$(wc -c < "$1")
     lsof "$1";
-    # if lsof returns an error code of 1, it means the file is not opened by any other processes, allowing us to move it safely
-    if [[ $? == 1 && $fileSize -gt 0 ]]; then
-        mv "$1" /home/airflow/gcs/data/;
+    # if lsof returns an error code of 1, it means the file is not opened by any other processes
+    if [ $? == 1 ]; then
+        fileSize=$(wc -c < "$1")
+        if [ $fileSize -gt 0 ]; then
+            mv "$1" /home/airflow/gcs/data/;
+        # if empty double check not waiting to be written to
+        else
+            sleep 1
+            lsof "$1";
+            if [ $? == 1 ]; then
+                mv "$1" /home/airflow/gcs/data/;
+            fi
+            return
+        fi
     fi
 }
 
