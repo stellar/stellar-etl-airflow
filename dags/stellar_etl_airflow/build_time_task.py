f39237ace2fb60bdef9bd7c9cad2ee7ee5d50038
sydneynotthecity <sydney@stellar.org>
2022-02-22 17:01:43 -0600

    Update kube configs to more optimal settings

--- dags/stellar_etl_airflow/build_time_task.py
+++ dags/stellar_etl_airflow/build_time_task.py
@@ -1,6 +1,7 @@
 '''
 This file contains functions for creating Airflow tasks to convert from a time range to a ledger range.
 '''
+import ast
 
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator 
 from airflow.models import Variable
@@ -38,6 +39,8 @@ def build_time_task(dag, use_testnet=False, use_next_exec_time=True):
          dag=dag,
          do_xcom_push=True,
          is_delete_operator_pod=True,
+         startup_timeout_seconds=720,
+         resources=ast.literal_eval(Variable.get('resources')),
          in_cluster=in_cluster,
          config_file=config_file_location,
          affinity=Variable.get('affinity', deserialize_json=True),
