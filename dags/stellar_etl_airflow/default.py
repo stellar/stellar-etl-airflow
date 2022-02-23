370d8beedc809733cda5d92a234386084a837f04
erika-sdf <92893480+erika-sdf@users.noreply.github.com>
2022-02-23 14:35:32 -0800

    Add sentry tags to dags for alerting (#90)

--- dags/stellar_etl_airflow/default.py
+++ dags/stellar_etl_airflow/default.py
@@ -1,5 +1,14 @@
 from datetime import timedelta, datetime
 from airflow.models import Variable
+import sentry_sdk
+from sentry_sdk import set_tag
+
+def init_sentry():
+    sentry_sdk.init(
+        dsn=Variable.get('sentry_dsn'),
+        environment=Variable.get('sentry_environment'),
+    )
+    set_tag("image_version", Variable.get('image_name'))
 
 def get_base_dag_args():
     return {
