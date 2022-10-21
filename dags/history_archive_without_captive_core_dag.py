dcd0349bccdb42a1204f97bd00d9dc99d1c0a41a
lucas zanotelli <lucas@zanotelli.dev>
2022-10-21 19:46:18 -0300

    correct partition boolean

--- dags/history_archive_without_captive_core_dag.py
+++ dags/history_archive_without_captive_core_dag.py
@@ -103,8 +103,8 @@ send_assets_to_pub_task = build_gcs_to_bq_task(dag, asset_export_task.task_id, p
 The tasks below use a job in BigQuery to deduplicate the table history_assets_stg.
 The job refreshes the table history_assets with only new records.
 '''
-dedup_assets_bq_task = build_bq_insert_job(dag, internal_project, internal_dataset, table_names['assets'], partition=True)
-dedup_assets_pub_task = build_bq_insert_job(dag, public_project, public_dataset, table_names['assets'], partition=False)
+dedup_assets_bq_task = build_bq_insert_job(dag, internal_project, internal_dataset, table_names['assets'], partition=False)
+dedup_assets_pub_task = build_bq_insert_job(dag, public_project, public_dataset, table_names['assets'], partition=True)
 
 time_task >> write_ledger_stats >> ledger_export_task >> delete_old_ledger_task >> send_ledgers_to_bq_task
 ledger_export_task >> delete_old_ledger_pub_task >> send_ledgers_to_pub_task
