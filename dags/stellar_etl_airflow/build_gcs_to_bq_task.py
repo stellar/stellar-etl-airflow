2f2e4d5e851c3f850912fb940ba121bf3f1888ea
sydneynotthecity <sydney@stellar.org>
2023-06-16 11:18:33 -0500

    Update Airflow task id to be unique

--- dags/stellar_etl_airflow/build_gcs_to_bq_task.py
+++ dags/stellar_etl_airflow/build_gcs_to_bq_task.py
@@ -90,6 +90,8 @@ def build_gcs_to_bq_task(
     project_name = project
     if dataset == Variable.get("public_dataset"):
         dataset_type = "pub"
+    if dataset == Variable.get("public_dataset_new"):
+        dataset_type = "pub_new"
     else:
         dataset_type = "bq"
     dataset_name = dataset
