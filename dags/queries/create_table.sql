bb24babf36b66ab560bc492240a7c82eacd1deec
Cayod <cayo.emanuel@gmail.com>
2023-07-07 14:06:05 -0300

    dag update for following airflow best practices

---
+++ dags/queries/create_table.sql
@@ -0,0 +1,7 @@
+create or replace table `{project_id}.{target_dataset}.{table_id}`
+partition by date_trunc(batch_run_date, month)
+options (partition_expiration_days = 180) as (
+    select *
+    from `{project_id}.{dataset_id}.{table_id}`
+    where batch_run_date >= date_sub(current_date(), interval 6 month)
+)
---
+++ dags/sandbox_create_dag.py
@@ -0,0 +1,88 @@
+"""
+This DAG creates the sandbox dataset with transactions tables, state tables with history and views.
+"""
+import datetime
+import json
+
+from airflow import DAG
+from airflow.models.variable import Variable
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from stellar_etl_airflow.build_bq_insert_job_task import (
+    file_to_string,
+    get_query_filepath,
+)
+from stellar_etl_airflow.default import (
+    alert_after_max_retries,
+    get_default_dag_args,
+    init_sentry,
+)
+
+init_sentry()
+
+with DAG(
+    "sandbox_create_dag",
+    default_args=get_default_dag_args(),
+    start_date=datetime.datetime(2023, 1, 1),
+    description="This DAG creates a sandbox",
+    schedule_interval="0 0 1 * *",
+    params={"alias": "sandbox_dataset"},
+    user_defined_filters={
+        "fromjson": lambda s: json.loads(s),
+    },
+    catchup=False,
+) as dag:
+    PROJECT = Variable.get("bq_project")
+    DATASET = Variable.get("bq_dataset")
+    SANDBOX_DATASET = Variable.get("sandbox_dataset")
+    DBT_DATASET = Variable.get("dbt_mart_dataset")
+    TABLES_ID = Variable.get("table_ids", deserialize_json=True)
+    DBT_TABLES = Variable.get("dbt_tables", deserialize_json=True)
+
+    start_tables_task = EmptyOperator(task_id="start_tables_task")
+    start_views_task = EmptyOperator(task_id="start_views_task")
+
+    query_path = get_query_filepath("create_table")
+    query = file_to_string(query_path)
+    for table_id in TABLES_ID:
+        sql_params = {
+            "project_id": PROJECT,
+            "dataset_id": DATASET,
+            "table_id": TABLES_ID[table_id],
+            "target_dataset": SANDBOX_DATASET,
+        }
+        query = query.format(**sql_params)
+        tables_create_task = BigQueryInsertJobOperator(
+            task_id=f"create_{table_id}",
+            configuration={
+                "query": {
+                    "query": query,
+                    "useLegacySql": False,
+                }
+            },
+            on_failure_callback=alert_after_max_retries,
+        )
+
+        start_tables_task >> tables_create_task
+
+    query_path = get_query_filepath("create_view")
+    query = file_to_string(query_path)
+    for dbt_table in DBT_TABLES:
+        sql_params = {
+            "project_id": PROJECT,
+            "dataset_id": DBT_DATASET,
+            "table_id": DBT_TABLES[dbt_table],
+            "target_dataset": SANDBOX_DATASET,
+        }
+        query = query.format(**sql_params)
+        dbt_tables_create_task = BigQueryInsertJobOperator(
+            task_id=f"create_{dbt_table}",
+            configuration={
+                "query": {
+                    "query": query,
+                    "useLegacySql": False,
+                }
+            },
+            on_failure_callback=alert_after_max_retries,
+        )
+        start_views_task >> dbt_tables_create_task
