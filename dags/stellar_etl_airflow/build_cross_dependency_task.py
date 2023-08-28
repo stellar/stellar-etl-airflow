3c67aeb40f9fbbb72d256e6a0b7aeeef251041cc
sydneynotthecity <sydney@stellar.org>
2023-08-28 14:36:17 -0500

    add time delta to correct dag

--- dags/stellar_etl_airflow/build_cross_dependency_task.py
+++ dags/stellar_etl_airflow/build_cross_dependency_task.py
@@ -3,12 +3,12 @@ import datetime
 from airflow.sensors.external_task import ExternalTaskSensor
 
 
-def build_cross_deps(dag, task, parent_dag, parent_task=None):
+def build_cross_deps(dag, task, parent_dag, parent_task=None, time_delta=0):
     return ExternalTaskSensor(
         task_id=f"check_{task}_finish",
         external_dag_id=parent_dag,
         external_task_id=parent_task,  # None means wait for the entire DAG to finish
-        execution_delta=datetime.timedelta(minutes=10),
+        execution_delta=datetime.timedelta(minutes=time_delta),
         timeout=3600,
         allowed_states=["success"],
         failed_states=["failed"],
