0e3cd4df09222461d160e3f3171cf078af44daec
sydneynotthecity <sydney@stellar.org>
2023-09-01 08:43:04 -0500

    Change sandbox schedule daily

--- dags/queries/create_table.sql
+++ dags/queries/create_table.sql
@@ -1,5 +1,5 @@
 create or replace table `{project_id}.{target_dataset}.{table_id}`
-partition by date_trunc(batch_run_date, month)
+partition by date_trunc(batch_run_date, day)
 options (partition_expiration_days = 180) as (
     select *
     from `{project_id}.{dataset_id}.{table_id}`
