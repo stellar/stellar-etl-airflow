8112f962c167e3ea2565041a405946daa565b0f9
Cayod <cayo.emanuel@gmail.com>
2023-09-27 08:47:49 -0300

    remove environment variable

--- dags/public_marts_tables_dag.py
+++ dags/public_marts_tables_dag.py
@@ -18,48 +18,40 @@ dag = DAG(
     catchup=False,
 )
 
-start_dag = DummyOperator(task_id="start_dag", dag=dag)
+# tasks for staging tables for marts
+stg_history_transactions = build_dbt_task(
+    dag, "stg_history_transactions", project="pub"
+)
+stg_history_ledgers = build_dbt_task(dag, "stg_history_ledgers", project="pub")
+stg_history_assets = build_dbt_task(dag, "stg_history_assets", project="pub")
+stg_history_operations = build_dbt_task(dag, "stg_history_operations", project="pub")
+# stg_history_trades = build_dbt_task(dag, "stg_history_trades", project="pub")
 
-start_dag
+# tasks for intermediate trades tables
+# int_trade_agg_day = build_dbt_task(dag, "int_trade_agg_day", project="pub")
+# int_trade_agg_month = build_dbt_task(dag, "int_trade_agg_month", project="pub")
+# int_trade_agg_week = build_dbt_task(dag, "int_trade_agg_week", project="pub")
+# int_trade_agg_year = build_dbt_task(dag, "int_trade_agg_year", project="pub")
 
-env = Variable.get("environment")
-if env == "prod":
-    # tasks for staging tables for marts
-    stg_history_transactions = build_dbt_task(
-        dag, "stg_history_transactions", project="pub"
-    )
-    stg_history_ledgers = build_dbt_task(dag, "stg_history_ledgers", project="pub")
-    stg_history_assets = build_dbt_task(dag, "stg_history_assets", project="pub")
-    stg_history_operations = build_dbt_task(
-        dag, "stg_history_operations", project="pub"
-    )
-    # stg_history_trades = build_dbt_task(dag, "stg_history_trades", project="pub")
+# tasks for marts tables
+# fee_stats_agg = build_dbt_task(dag, "fee_stats_agg", project="pub")
+# trade_agg = build_dbt_task(dag, "trade_agg", project="pub")
+history_assets = build_dbt_task(dag, "history_assets", project="pub")
+enriched_history = build_dbt_task(dag, "enriched_history_operations", project="pub")
 
-    # tasks for intermediate trades tables
-    # int_trade_agg_day = build_dbt_task(dag, "int_trade_agg_day", project="pub")
-    # int_trade_agg_month = build_dbt_task(dag, "int_trade_agg_month", project="pub")
-    # int_trade_agg_week = build_dbt_task(dag, "int_trade_agg_week", project="pub")
-    # int_trade_agg_year = build_dbt_task(dag, "int_trade_agg_year", project="pub")
+# DAG task graph
+# graph for marts tables
 
-    # tasks for marts tables
-    # fee_stats_agg = build_dbt_task(dag, "fee_stats_agg", project="pub")
-    # trade_agg = build_dbt_task(dag, "trade_agg", project="pub")
-    history_assets = build_dbt_task(dag, "history_assets", project="pub")
-    enriched_history = build_dbt_task(dag, "enriched_history_operations", project="pub")
+# stg_history_transactions >> fee_stats_agg
+# stg_history_ledgers >> fee_stats_agg
 
-    # DAG task graph
-    # graph for marts tables
+stg_history_transactions >> enriched_history
+stg_history_ledgers >> enriched_history
+stg_history_operations >> enriched_history
 
-    # stg_history_transactions >> fee_stats_agg
-    # stg_history_ledgers >> fee_stats_agg
+stg_history_assets >> history_assets
 
-    stg_history_transactions >> enriched_history
-    stg_history_ledgers >> enriched_history
-    stg_history_operations >> enriched_history
-
-    stg_history_assets >> history_assets
-
-    # stg_history_trades >> int_trade_agg_day >> trade_agg
-    # stg_history_trades >> int_trade_agg_month >> trade_agg
-    # stg_history_trades >> int_trade_agg_week >> trade_agg
-    # stg_history_trades >> int_trade_agg_year >> trade_agg
+# stg_history_trades >> int_trade_agg_day >> trade_agg
+# stg_history_trades >> int_trade_agg_month >> trade_agg
+# stg_history_trades >> int_trade_agg_week >> trade_agg
+# stg_history_trades >> int_trade_agg_year >> trade_agg
