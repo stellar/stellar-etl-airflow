59a6e4f1c1212f0336ebe2bd09a14b45ad48a007
Simon Chow <simon.chow@stellar.org>
2023-10-06 13:49:54 -0400

    Update asset pricing dag; remove the delete tasks

--- dags/asset_pricing_pipeline_dag.py
+++ dags/asset_pricing_pipeline_dag.py
@@ -15,7 +15,6 @@ dag = DAG(
     start_date=datetime.datetime(2023, 9, 10),
     description="This DAG runs dbt to calculate asset pricing based on stablecoin and XLM trades",
     schedule_interval="0 2 * * *",  # daily at 2am
-    catchup=True,
     params={},
     user_defined_filters={"fromjson": lambda s: json.loads(s)},
     user_defined_macros={
@@ -32,27 +31,6 @@ dbt_marts = Variable.get("sdf_marts")
 stg_history_trades = build_dbt_task(dag, "stg_history_trades")
 stg_asset_prices_usd = build_dbt_task(dag, "stg_asset_prices_usd")
 
-# tasks for deleting old data in case of reruns
-delete_int_usdc_trades = build_delete_dbt_data_task(
-    dag, internal_project, dbt_conformed, "int_usdc_trades", "closed_at"
-)
-
-delete_int_usds_trades = build_delete_dbt_data_task(
-    dag, internal_project, dbt_conformed, "int_usds_trades", "closed_at"
-)
-
-delete_int_xlm_trades = build_delete_dbt_data_task(
-    dag, internal_project, dbt_conformed, "int_xlm_trades", "closed_at"
-)
-
-delete_int_stable_coin_prices = build_delete_dbt_data_task(
-    dag, internal_project, dbt_conformed, "int_stable_coin_prices", "closed_date"
-)
-
-delete_ohlc_exchange_fact = build_delete_dbt_data_task(
-    dag, internal_project, dbt_marts, "ohlc_exchange_fact", "closed_date"
-)
-
 # tasks for ohlc intermediate tables
 int_stable_coin_prices = build_dbt_task(dag, "int_stable_coin_prices")
 int_usdc_trades = build_dbt_task(dag, "int_usdc_trades")
@@ -63,10 +41,7 @@ int_xlm_trades = build_dbt_task(dag, "int_xlm_trades")
 ohlc_exchange_fact = build_dbt_task(dag, "ohlc_exchange_fact")
 
 # DAG task graph
-stg_history_trades >> delete_int_usdc_trades >> int_usdc_trades >> delete_ohlc_exchange_fact
-stg_history_trades >> delete_int_usds_trades >> int_usds_trades >> delete_ohlc_exchange_fact
-stg_history_trades >> delete_int_xlm_trades >> int_xlm_trades >> delete_ohlc_exchange_fact
-int_xlm_trades >> delete_int_stable_coin_prices >> int_stable_coin_prices >> delete_ohlc_exchange_fact
-
-stg_asset_prices_usd >> delete_ohlc_exchange_fact
-delete_ohlc_exchange_fact >> ohlc_exchange_fact
+stg_history_trades >> int_usdc_trades >> ohlc_exchange_fact
+stg_history_trades >> int_usds_trades >> ohlc_exchange_fact
+stg_history_trades >> int_xlm_trades >> ohlc_exchange_fact
+int_xlm_trades >> int_stable_coin_prices >> ohlc_exchange_fact
