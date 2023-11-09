462e5fe6991116bcadc8c57954788482761387fe
Simon Chow <simon.chow@stellar.org>
2023-11-09 02:13:25 -0500

    linting

--- dags/soroban_tables_dag.py
+++ dags/soroban_tables_dag.py
@@ -27,22 +27,30 @@ stg_expiration = build_dbt_task(dag, "stg_expiration")
 
 # tasks for intermediate soroban aggregate tables
 int_soroban__contract_metrics_day = build_dbt_task(
-    dag, "int_soroban__contract_metrics_day")
+    dag, "int_soroban__contract_metrics_day"
+)
 int_soroban__contract_metrics_day = build_dbt_task(
-    dag, "int_soroban__contract_metrics_week")
+    dag, "int_soroban__contract_metrics_week"
+)
 int_soroban__contract_metrics_day = build_dbt_task(
-    dag, "int_soroban__contract_metrics_month")
+    dag, "int_soroban__contract_metrics_month"
+)
 int_soroban__contract_metrics_day = build_dbt_task(
-    dag, "int_soroban__contract_metrics_year")
+    dag, "int_soroban__contract_metrics_year"
+)
 
 int_soroban__top_contract_activity_day = build_dbt_task(
-    dag, "int_soroban__top_contract_activity_day")
+    dag, "int_soroban__top_contract_activity_day"
+)
 int_soroban__top_contract_activity_week = build_dbt_task(
-    dag, "int_soroban__top_contract_activity_week")
+    dag, "int_soroban__top_contract_activity_week"
+)
 int_soroban__top_contract_activity_month = build_dbt_task(
-    dag, "int_soroban__top_contract_activity_month")
+    dag, "int_soroban__top_contract_activity_month"
+)
 int_soroban__top_contract_activity_year = build_dbt_task(
-    dag, "int_soroban__top_contract_activity_year")
+    dag, "int_soroban__top_contract_activity_year"
+)
 
 # tasks for marts tables
 contract_assets_fact = build_dbt_task(dag, "contract_assets_fact")
@@ -50,7 +58,8 @@ contract_type_fact = build_dbt_task(dag, "contract_type_fact")
 expiration_current = build_dbt_task(dag, "expiration_current")
 soroban__contract_metrics_agg = build_dbt_task(dag, "soroban__contract_metrics_agg")
 soroban__top_contract_activity_agg = build_dbt_task(
-    dag, "soroban__top_contract_activity_agg")
+    dag, "soroban__top_contract_activity_agg"
+)
 
 # DAG task graph
 stg_config_settings
@@ -69,4 +78,3 @@ int_soroban__top_contract_activity_day >> soroban__top_contract_activity_agg
 int_soroban__top_contract_activity_week >> soroban__top_contract_activity_agg
 int_soroban__top_contract_activity_month >> soroban__top_contract_activity_agg
 int_soroban__top_contract_activity_year >> soroban__top_contract_activity_agg
-
