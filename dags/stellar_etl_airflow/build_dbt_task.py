84d8333b64bc7d32d766df3d31ae24d206063e74
sydneynotthecity <sydney@stellar.org>
2024-01-23 11:13:22 -0600

    Update build_dbt_task.py

--- dags/stellar_etl_airflow/build_dbt_task.py
+++ dags/stellar_etl_airflow/build_dbt_task.py
@@ -111,7 +111,7 @@ def dbt_task(
         service_account_name=Variable.get("k8s_service_account"),
         env_vars={
             "DBT_USE_COLORS": "0",
-            "DBT_DATASET": "{{ var.value.dbt_dataset }}"
+            "DBT_DATASET": "{{ var.value.dbt_dataset }}",
             "DBT_TARGET": "{{ var.value.dbt_target }}",
             "DBT_MAX_BYTES_BILLED": "{{ var.value.dbt_maximum_bytes_billed }}",
             "DBT_JOB_TIMEOUT": "{{ var.value.dbt_job_execution_timeout_seconds }}",
