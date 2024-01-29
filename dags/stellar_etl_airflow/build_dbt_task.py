0502f57b0abd05fccf6854cb2ec31f2841b86994
sydneynotthecity <sydney@stellar.org>
2024-01-29 15:13:50 -0600

    Fix formatting

--- dags/stellar_etl_airflow/build_dbt_task.py
+++ dags/stellar_etl_airflow/build_dbt_task.py
@@ -121,7 +121,7 @@ def dbt_task(
             "INTERNAL_SOURCE_DB": "{{ var.value.internal_source_db }}",
             "INTERNAL_SOURCE_SCHEMA": "{{ var.value.internal_source_schema }}",
             "PUBLIC_SOURCE_DB": "{{ var.value.public_source_db }}",
-            "PUBLIC_SOURCE_SCHEMA": "{{ var.value.public_source_schema }}" 
+            "PUBLIC_SOURCE_SCHEMA": "{{ var.value.public_source_schema }}",
         },
         image=dbt_image,
         arguments=args,
