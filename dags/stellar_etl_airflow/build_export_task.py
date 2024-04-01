d2639a8d168e001581571094f8d923e3a00c2ff9
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-01 20:52:56 -0300

    added successful transforms in return value

--- dags/stellar_etl_airflow/build_export_task.py
+++ dags/stellar_etl_airflow/build_export_task.py
@@ -198,7 +198,8 @@ def build_export_task(
     else:
         arguments = f"""
                     {etl_cmd_string} 2>> stderr.out && echo "{{\\"output\\": \\"{output_file}\\",
-                    \\"failed_transforms\\": `grep failed_transforms stderr.out | cut -d\\",\\" -f2 | cut -d\\":\\" -f2`}}" >> /airflow/xcom/return.json
+                    \\"failed_transforms\\": `grep failed_transforms stderr.out | cut -d\\",\\" -f2 | cut -d\\":\\" -f2`}}" >> /airflow/xcom/return.json,
+                    \\"succcessful_transforms\\": `grep successful_transforms stderr.out | cut -d\\",\\" -f2 | cut -d\\":\\" -f2`}}" >> /airflow/xcom/return.json
                     """
     return KubernetesPodOperator(
         service_account_name=Variable.get("k8s_service_account"),
