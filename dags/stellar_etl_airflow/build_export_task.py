64fabbc6e45e3bd807f72b94f71cb99e1e2e4b9f
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-03 15:29:20 -0300

    with jq -r command

--- dags/stellar_etl_airflow/build_export_task.py
+++ dags/stellar_etl_airflow/build_export_task.py
@@ -214,8 +214,8 @@ def build_export_task(
     else:
         arguments = f"""
                     {etl_cmd_string} 2>> stderr.out && echo "{{\\"output\\": \\"{output_file}\\",
-                    \\"whole message\\": \\"$(cat stderr.out)\\",
-                    \\"failed_transforms\\": `grep failed_transforms stderr.out | cut -d\\",\\" -f2 | cut -d\\":\\" -f2`}}" >> /airflow/xcom/return.json
+                    \\"successful_transforms\\": `grep successful_transforms stderr.out | jq -r .successful_transforms`",
+                    \\"failed_transforms\\": `grep failed_transforms stderr.out | cut -d\\",\\" -f2 | cut -d\\":\\" -f2`"}}" >> /airflow/xcom/return.json
                     """
     return KubernetesPodOperator(
         service_account_name=Variable.get("k8s_service_account"),
