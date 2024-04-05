96ffc6c750d9a9daec0c63baefad8f074509e10f
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-05 14:10:38 -0300

    correct format

--- dags/stellar_etl_airflow/build_export_task.py
+++ dags/stellar_etl_airflow/build_export_task.py
@@ -216,7 +216,9 @@ def build_export_task(
                     {0} 2>> stderr.out && cat stderr.out && echo "{{\\"output\\": \\"{1}\\",
                     \\"failed_transforms\\": `grep failed_transforms stderr.out | cut -d\\",\\" -f2 | cut -d\\":\\" -f2`,
                     \\"successful_transforms\\: `grep successful_transforms stderr.out | cut -d\\":\\" -f3 | cut -d\\":\\" -f2 | tr -d '}}' `}}" >> /airflow/xcom/return.json
-                    """.format(etl_cmd_string, output_file)
+                    """.format(
+                        etl_cmd_string, output_file
+                    )
     return KubernetesPodOperator(
         service_account_name=Variable.get("k8s_service_account"),
         namespace=Variable.get("k8s_namespace"),
