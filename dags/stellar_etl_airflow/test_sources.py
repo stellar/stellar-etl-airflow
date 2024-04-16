0bd5f473cdc39f535d3add3c47484280015c33ce
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-16 18:58:11 -0300

    test test_sources ledgers

---
+++ dags/stellar_etl_airflow/test_sources.py
@@ -0,0 +1,78 @@
+from airflow.operators.python_operator import PythonOperator
+from google.cloud import bigquery
+from airflow.models import TaskInstance, DagRun, DagBag
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.models import Variable
+from google.oauth2 import service_account
+import json
+
+def compare_transforms_and_bq_rows():
+
+    # current_date is always today - 1
+    current_date = datetime.now() - timedelta(days=1)
+    current_date = current_date.date()
+    print("Current date is: ", current_date)
+
+    # Get all the execution dates for the current date
+    execution_dates = DagRun.find(dag_id='history_archive_without_captive_core', execution_date=current_date)
+    print(f"Execution dates: {execution_dates}")
+
+    # Get the DAG
+    dag_bag = DagBag()
+    dag = dag_bag.get_dag('history_archive_without_captive_core')
+
+    # Get the task
+    task = dag.get_task('export_ledgers_task')
+
+    total_successful_transforms = 0
+
+    for execution_date in execution_dates:
+        # Retrieve successful_transforms from XCOM
+        ti = TaskInstance(task, execution_date)
+        xcom_ledgers = ti.xcom_pull(task_ids=task)
+
+        # Parse JSON and get successful_transforms
+        successful_transforms_ledgers = json.loads(xcom_ledgers)['successful_transforms']
+        total_successful_transforms += successful_transforms_ledgers
+
+    print(f'Total successful transforms for {current_date}: {total_successful_transforms}')
+
+    # key_path = Variable.get("api_key_path")
+    # credentials = service_account.Credentials.from_service_account_file(key_path)
+    # client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+    # # Query number of rows in BigQuery table
+    # query_job = client.query(f"SELECT
+    #     (SELECT COUNT(*) FROM crypto-stellar.crypto_stellar.history_ledgers
+    #     WHERE batch_run_date BETWEEN '2020-12-04' AND '2020-12-05') AS count_public,
+    #     "
+    # )
+    
+    # results = query_job.result()
+    # bq_rows = [row for row in results][0][0]
+
+    # # Compare successful_transforms and bq_rows
+    # if successful_transforms_ledgers != bq_rows:
+    #     raise ValueError('Mismatch between successful_transforms in ledgers and bq_rows')
+
+dag = DAG(
+    'daily_test_sources',
+    start_date=datetime(2024, 4, 15),
+    schedule_interval=timedelta(days=1),
+    default_args={
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+)
+
+compare_task = PythonOperator(
+    task_id='compare_transforms_and_bq_rows',
+    python_callable=compare_transforms_and_bq_rows,
+    provide_context=True,
+    dag=dag,
+)
+
+
+
+
