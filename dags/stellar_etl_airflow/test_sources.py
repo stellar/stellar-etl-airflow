37c5b96f2209b6e50028fa7b4ad396ef2427fd52
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-17 13:47:44 -0300

    python dict

--- dags/stellar_etl_airflow/test_sources.py
+++ dags/stellar_etl_airflow/test_sources.py
@@ -32,9 +32,8 @@ def compare_transforms_and_bq_rows():
         xcom_ledgers = ti.xcom_pull(task_ids=task.task_id, key="return_value")
 
         # Parse JSON and get successful_transforms
-        successful_transforms_ledgers = json.loads(xcom_ledgers)[
-            "successful_transforms"
-        ]
+        successful_transforms_ledgers = xcom_ledgers["successful_transforms"]
+        
         total_successful_transforms += successful_transforms_ledgers
 
     print(f"Total successful transforms for yesterday: {total_successful_transforms}")
