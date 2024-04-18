c3463033585315a45da1b0ed0a0011fca3bd5c79
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-18 20:01:45 -0300

    pendulum.instance

--- dags/stellar_etl_airflow/test_sources.py
+++ dags/stellar_etl_airflow/test_sources.py
@@ -10,7 +10,15 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 
 
-def get_from_with_combinedExport():
+def get_from_combinedExport():
+    
+    successful_transforms = {
+        'operations': None,
+        'trades': None,
+        'effects': None,
+        'transactions': None,
+    }
+
     # yesterday = pendulum.datetime(2024, 4, 16, tz="UTC")
     # Get all the execution dates for the current date
     # Get the session from the settings
@@ -44,7 +52,7 @@ def get_from_with_combinedExport():
     lines = file_content.splitlines()
 
     total_successful_transforms = 0
-
+    i = 0
     for line in lines:
         if 'level=info msg="{\\' in line:
             start = line.find('{\\"')
@@ -61,15 +69,20 @@ def get_from_with_combinedExport():
 
             # Slice the string to get the value between the last colon and the closing brace
             value = json_str[last_colon + 1 : closing_brace]
-            total_successful_transforms += int(value)
+            
+            for key in successful_transforms:
+                if successful_transforms[key] is None:
+                    successful_transforms[key] = int(value)
+                    break
 
     print(f"Total successful transforms for yesterday: {total_successful_transforms}")
 
 
-def get_from_without_captiveCore():
+def get_from_without_captiveCore(**context):
     # Try yesterday_ds again
-    yesterday = pendulum.yesterday()
-    # Get all the execution dates for the current date
+    execution_date = context['execution_date']
+    yesterday = pendulum.instance(execution_date).subtract(days=1)
+    
     # Get the session from the settings
     session = settings.Session()
 
@@ -148,8 +161,8 @@ dag = DAG(
 # )
 
 compare2_task = PythonOperator(
-    task_id="get_from_with_combinedExport",
-    python_callable=get_from_with_combinedExport,
+    task_id="get_from_combinedExport",
+    python_callable=get_from_combinedExport,
     provide_context=True,
     dag=dag,
 )
