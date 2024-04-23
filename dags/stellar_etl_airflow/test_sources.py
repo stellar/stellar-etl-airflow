0f5d5347099d44716b9945b272e4a7b17e1557fd
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-23 16:38:33 -0300

    2024-04-23T16:30:00+00:00

--- dags/stellar_etl_airflow/test_sources.py
+++ dags/stellar_etl_airflow/test_sources.py
@@ -175,19 +175,20 @@ def get_from_historyTableExport(**context):
     session = settings.Session()
 
     # Get all the execution dates for the current date (yesterday)
-    execution_dates = (
-        session.query(DagRun)
-        .filter(
-            DagRun.dag_id == "history_table_export",
-            DagRun.execution_date >= yesterday,
-            DagRun.execution_date
-            < datetime.combine(
-                yesterday + timedelta(days=1), time(), tzinfo=pytz.timezone("UTC")
-            ),
-            DagRun.state == State.SUCCESS,
-        )
-        .all()
-    )
+    # execution_dates = (
+    #     session.query(DagRun)
+    #     .filter(
+    #         DagRun.dag_id == "history_table_export",
+    #         DagRun.execution_date >= yesterday,
+    #         DagRun.execution_date
+    #         < datetime.combine(
+    #             yesterday + timedelta(days=1), time(), tzinfo=pytz.timezone("UTC")
+    #         ),
+    #         DagRun.state == State.SUCCESS,
+    #     )
+    #     .all()
+    # )
+    execution_dates = ["2024-04-23T16:30:00+00:00"]
 
     dag_bag = DagBag()
     dag = dag_bag.get_dag("history_table_export")
