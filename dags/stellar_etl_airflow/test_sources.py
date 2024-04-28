284abe996f4e6f2f5e787a5bf8d310b20e5ca99f
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-04-28 16:41:22 -0300

    - third day

--- dags/stellar_etl_airflow/test_sources.py
+++ dags/stellar_etl_airflow/test_sources.py
@@ -57,7 +57,7 @@ def treating_errors(successful_transforms, BQ_results):
 
 def comparison(**context):
     execution_date = context["execution_date"]
-    yesterday = pendulum.instance(execution_date).subtract(days=2)
+    yesterday = pendulum.instance(execution_date).subtract(days=3)
 
     # Define the table names
     tables1 = ["ledgers", "operations", "trades", "effects", "transactions"]
