a25c8de0041f8b8ca2e1b58b4fc6be2d33f70083
sydneynotthecity <sydney@stellar.org>
2024-05-01 21:24:10 -0500

    Configure sqlfluff template vars

--- dags/queries/update_table.sql
+++ dags/queries/update_table.sql
@@ -1,4 +1,4 @@
-insert into {target_project}.{target_dataset}.{table_id}
+insert into `{target_project}.{target_dataset}.{table_id}`
 select *
-from {project_id}.{dataset_id}.{table_id}
+from `{project_id}.{dataset_id}.{table_id}`
 where date_trunc(batch_run_date, day) = date_trunc(cast('{batch_run_date}' as datetime), day)
