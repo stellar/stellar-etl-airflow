95b9a301a77d21074f986cd7d989d13334a5d12b
Laysa de Sousa Bitencourt <laysa.bitencourt@indicium.tech>
2024-05-16 14:59:44 -0300

    requirements updated

--- dags/queries/enriched_meaningful_history_operations.sql
+++ dags/queries/enriched_meaningful_history_operations.sql
@@ -13,11 +13,12 @@ select
     , ma.issuer
 from {project_id}.{dataset_id}.enriched_history_operations as eho
 inner join
-    {project_id}.{dataset_id}.meaningful_assets as ma on
-eho.asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
-or eho.source_asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
-or eho.selling_asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
-or eho.buying_asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
+    {project_id}.{dataset_id}.meaningful_assets as ma
+    on
+    eho.asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
+    or eho.source_asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
+    or eho.selling_asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
+    or eho.buying_asset_id = farm_fingerprint(concat(ma.code, ma.issuer, ma.type))
 where
     eho.batch_id = '{batch_id}'
     and eho.batch_run_date = '{batch_run_date}'
