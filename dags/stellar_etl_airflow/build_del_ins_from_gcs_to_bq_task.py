3efd5c7edb8268efaed4d677672638da43a18344
harsha-stellar-data <sriharsha.pasupuleti@stellar.org>
2024-07-08 11:26:34 -0600

    Modified the code to account for the pre-commit changes

--- dags/stellar_etl_airflow/build_del_ins_from_gcs_to_bq_task.py
+++ dags/stellar_etl_airflow/build_del_ins_from_gcs_to_bq_task.py
@@ -1,15 +1,16 @@
 from datetime import timedelta
+
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
+    GCSToBigQueryOperator,
+)
+from sentry_sdk import capture_message, push_scope
 from stellar_etl_airflow import macros
 from stellar_etl_airflow.build_apply_gcs_changes_to_bq_task import read_local_schema
 from stellar_etl_airflow.default import alert_after_max_retries
-from sentry_sdk import capture_message, push_scope
 
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from sentry_sdk import capture_message, push_scope
 
 class CustomGCSToBigQueryOperator(GCSToBigQueryOperator):
     template_fields = list(GCSToBigQueryOperator.template_fields) + [
@@ -18,7 +19,9 @@ class CustomGCSToBigQueryOperator(GCSToBigQueryOperator):
         "export_task_id",
     ]
 
-    def __init__(self, failed_transforms, max_failed_transforms, export_task_id, **kwargs):
+    def __init__(
+        self, failed_transforms, max_failed_transforms, export_task_id, **kwargs
+    ):
         self.failed_transforms = failed_transforms
         self.max_failed_transforms = max_failed_transforms
         self.export_task_id = export_task_id
@@ -27,7 +30,9 @@ class CustomGCSToBigQueryOperator(GCSToBigQueryOperator):
     def pre_execute(self, context, **kwargs):
         # Expand the template fields using Jinja2 templating from the context
         self.failed_transforms = self.render_template(self.failed_transforms, context)
-        self.max_failed_transforms = self.render_template(self.max_failed_transforms, context)
+        self.max_failed_transforms = self.render_template(
+            self.max_failed_transforms, context
+        )
         self.export_task_id = self.render_template(self.export_task_id, context)
 
         if int(self.failed_transforms) > self.max_failed_transforms:
@@ -37,7 +42,9 @@ class CustomGCSToBigQueryOperator(GCSToBigQueryOperator):
                 scope.set_extra("file", f"gs://{self.bucket}/{self.source_objects[0]}")
                 scope.set_extra("task_id", self.task_id)
                 scope.set_extra("export_task_id", self.export_task_id)
-                scope.set_extra("destination_table", self.destination_project_dataset_table)
+                scope.set_extra(
+                    "destination_table", self.destination_project_dataset_table
+                )
                 capture_message(
                     f"failed_transforms ({self.failed_transforms}) has exceeded the max value ({self.max_failed_transforms})",
                     "fatal",
@@ -45,9 +52,20 @@ class CustomGCSToBigQueryOperator(GCSToBigQueryOperator):
         super().pre_execute(context, **kwargs)
 
 
-
-def build_del_ins_from_gcs_to_bq_task(project, dataset, data_type, export_task_id, source_object_suffix, partition, cluster, batch_id ,batch_date, source_objects, **context):
-    dag = context['dag']
+def build_del_ins_from_gcs_to_bq_task(
+    project,
+    dataset,
+    data_type,
+    export_task_id,
+    source_object_suffix,
+    partition,
+    cluster,
+    batch_id,
+    batch_date,
+    source_objects,
+    **context,
+):
+    dag = context["dag"]
 
     # Delete operation
 
@@ -61,7 +79,7 @@ def build_del_ins_from_gcs_to_bq_task(project, dataset, data_type, export_task_i
         task_id=f"delete_old_partition_{data_type}_bq",
         execution_timeout=timedelta(
             seconds=Variable.get("task_timeout", deserialize_json=True)[
-               build_del_ins_from_gcs_to_bq_task.__name__
+                build_del_ins_from_gcs_to_bq_task.__name__
             ]
         ),
         on_failure_callback=alert_after_max_retries,
@@ -80,7 +98,7 @@ def build_del_ins_from_gcs_to_bq_task(project, dataset, data_type, export_task_i
     delete_task.execute(context)
 
     # Insert operation
-    
+
     bucket_name = Variable.get("gcs_exported_data_bucket_name")
     history_tables = [
         "ledgers",
@@ -90,7 +108,7 @@ def build_del_ins_from_gcs_to_bq_task(project, dataset, data_type, export_task_i
         "trades",
         "effects",
         "contract_events",
-    ]    
+    ]
 
     if cluster:
         cluster_fields = Variable.get("cluster_fields", deserialize_json=True)
@@ -131,11 +149,11 @@ def build_del_ins_from_gcs_to_bq_task(project, dataset, data_type, export_task_i
             schema_update_options=["ALLOW_FIELD_ADDITION"],
             autodetect=False,
             source_format="NEWLINE_DELIMITED_JSON",
-            source_objects = source_objects,
+            source_objects=source_objects,
             destination_project_dataset_table=f"{project}.{dataset}.{data_type}{staging_table_suffix}",
             write_disposition="WRITE_APPEND",
             create_disposition="CREATE_IF_NEEDED",
-            #schema_update_option="ALLOW_FIELD_ADDITION",
+            # schema_update_option="ALLOW_FIELD_ADDITION",
             max_bad_records=0,
             time_partitioning=time_partition,
             cluster_fields=cluster_fields,
@@ -166,7 +184,7 @@ def build_del_ins_from_gcs_to_bq_task(project, dataset, data_type, export_task_i
             schema_update_options=["ALLOW_FIELD_ADDITION"],
             autodetect=False,
             source_format="NEWLINE_DELIMITED_JSON",
-            source_objects = source_objects,
+            source_objects=source_objects,
             destination_project_dataset_table=f"{project}.{dataset}.{data_type}{staging_table_suffix}",
             write_disposition="WRITE_APPEND",
             create_disposition="CREATE_IF_NEEDED",
