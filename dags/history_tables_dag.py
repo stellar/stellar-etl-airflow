0467cff0f11230c60f66230fbb82f840831a3db2
harsha-stellar-data <sriharsha.pasupuleti@stellar.org>
2024-07-08 11:26:34 -0600

    Modified the code to account for the pre-commit changes

--- dags/history_tables_dag.py
+++ dags/history_tables_dag.py
@@ -2,28 +2,28 @@
 The history_archive_export DAG exports trades, asstes, ledgers, operations, transactions, effects from the history archives.
 It is scheduled to export information to BigQuery at regular intervals.
 """
+
 from ast import literal_eval
 from datetime import datetime
 from json import loads
 
 from airflow import DAG
 from airflow.models.variable import Variable
+from airflow.operators.python import PythonOperator
 from kubernetes.client import models as k8s
 from stellar_etl_airflow import macros
-from airflow.operators.python import PythonOperator
-
 from stellar_etl_airflow.build_batch_stats import build_batch_stats
 from stellar_etl_airflow.build_bq_insert_job_task import build_bq_insert_job
 from stellar_etl_airflow.build_cross_dependency_task import build_cross_deps
+from stellar_etl_airflow.build_del_ins_from_gcs_to_bq_task import (
+    build_del_ins_from_gcs_to_bq_task,
+)
 from stellar_etl_airflow.build_delete_data_task import build_delete_data_task
 from stellar_etl_airflow.build_export_task import build_export_task
 from stellar_etl_airflow.build_gcs_to_bq_task import build_gcs_to_bq_task
 from stellar_etl_airflow.build_time_task import build_time_task
-from stellar_etl_airflow.build_del_ins_from_gcs_to_bq_task import build_del_ins_from_gcs_to_bq_task
-
-from stellar_etl_airflow.default import alert_after_max_retries
-
 from stellar_etl_airflow.default import (
+    alert_after_max_retries,
     alert_sla_miss,
     get_default_dag_args,
     init_sentry,
@@ -52,14 +52,14 @@ dag = DAG(
         "subtract_data_interval": macros.subtract_data_interval,
         "batch_run_date_as_datetime_string": macros.batch_run_date_as_datetime_string,
     },
-    #sla_miss_callback=alert_sla_miss,
+    # sla_miss_callback=alert_sla_miss,
 )
 
-#fetch and set batch metadata variables
+# fetch and set batch metadata variables
 batch_id = macros.get_batch_id()
 batch_date = "{{ batch_run_date_as_datetime_string(dag, data_interval_start) }}"
 
-source_object_suffix=""
+source_object_suffix = ""
 
 # Fetch necessary variables
 table_names = Variable.get("table_ids", deserialize_json=True)
@@ -180,7 +180,7 @@ contract_events_export_task = build_export_task(
 )
 
 
-#define the variables needed for making the call to the Operator that does the Delete from BQ Table and Inserts data from GCS to BigQuery
+# define the variables needed for making the call to the Operator that does the Delete from BQ Table and Inserts data from GCS to BigQuery
 
 ops_export_task_id = op_export_task.task_id
 trades_export_task_id = trade_export_task.task_id
@@ -190,49 +190,49 @@ ledgers_export_task_id = ledger_export_task.task_id
 assets_export_task_id = asset_export_task.task_id
 cont_evnts_export_task_id = contract_events_export_task.task_id
 
-ops_source_objects=[
+ops_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + ops_export_task_id
     + '\')["output"] }}'
     + source_object_suffix
 ]
 
-trade_source_objects=[
+trade_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + trades_export_task_id
     + '\')["output"] }}'
     + source_object_suffix
 ]
 
-effects_source_objects=[
+effects_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + effects_export_task_id
     + '\')["output"] }}'
     + source_object_suffix
 ]
 
-txns_source_objects=[
+txns_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + txns_export_task_id
     + '\')["output"] }}'
     + source_object_suffix
 ]
 
-ledger_source_objects=[
+ledger_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + ledgers_export_task_id
     + '\')["output"] }}'
     + source_object_suffix
 ]
 
-asset_source_objects=[
+asset_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + assets_export_task_id
     + '\')["output"] }}'
     + source_object_suffix
 ]
 
-cont_evnts_source_objects=[
+cont_evnts_source_objects = [
     "{{ task_instance.xcom_pull(task_ids='"
     + cont_evnts_export_task_id
     + '\')["output"] }}'
@@ -243,14 +243,25 @@ cont_evnts_source_objects=[
 The task calls the Custom Python Task that does the following operations in sequence
     1. The delete operation in the task checks to see if the given partition/batch id exists in
        Bigquery. If it does, the records are deleted prior to reinserting the batch.
-    2. The Insert operation in the tasks receive the location of the file in Google Cloud storage 
+    2. The Insert operation in the tasks receive the location of the file in Google Cloud storage
     through Airflow's XCOM system and merges the unique entries in the file into the corresponding table in BigQuery.
 """
 
 del_ins_ops_task = PythonOperator(
     task_id=f"del_ins_ops_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["operations"], ops_export_task_id, source_object_suffix, True, True, batch_id, batch_date, ops_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["operations"],
+        ops_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        ops_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -258,7 +269,18 @@ del_ins_ops_task = PythonOperator(
 del_ins_trades_task = PythonOperator(
     task_id=f"del_ins_trades_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["trades"], trades_export_task_id, source_object_suffix, True, True, batch_id, batch_date, trade_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["trades"],
+        trades_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        trade_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -266,7 +288,18 @@ del_ins_trades_task = PythonOperator(
 del_ins_effects_task = PythonOperator(
     task_id=f"del_ins_effects_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["effects"], effects_export_task_id, source_object_suffix, True, True, batch_id, batch_date, effects_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["effects"],
+        effects_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        effects_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -274,7 +307,18 @@ del_ins_effects_task = PythonOperator(
 del_ins_txns_task = PythonOperator(
     task_id=f"del_ins_txns_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["transactions"], txns_export_task_id, source_object_suffix, True, True, batch_id, batch_date, txns_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["transactions"],
+        txns_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        txns_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -282,7 +326,18 @@ del_ins_txns_task = PythonOperator(
 del_ins_ledgers_task = PythonOperator(
     task_id=f"del_ins_ledgers_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["ledgers"], ledgers_export_task_id, source_object_suffix, True, True, batch_id, batch_date, ledger_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["ledgers"],
+        ledgers_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        ledger_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -290,7 +345,18 @@ del_ins_ledgers_task = PythonOperator(
 del_ins_assets_task = PythonOperator(
     task_id=f"del_ins_assets_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["assets"], assets_export_task_id, source_object_suffix, True, True, batch_id, batch_date, asset_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["assets"],
+        assets_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        asset_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -298,7 +364,18 @@ del_ins_assets_task = PythonOperator(
 del_ins_cont_events_task = PythonOperator(
     task_id=f"del_ins_cont_evnts_task",
     python_callable=build_del_ins_from_gcs_to_bq_task,
-    op_args=[public_project, public_dataset, table_names["contract_events"], cont_evnts_export_task_id, source_object_suffix, True, True, batch_id, batch_date, cont_evnts_source_objects],
+    op_args=[
+        public_project,
+        public_dataset,
+        table_names["contract_events"],
+        cont_evnts_export_task_id,
+        source_object_suffix,
+        True,
+        True,
+        batch_id,
+        batch_date,
+        cont_evnts_source_objects,
+    ],
     provide_context=True,
     dag=dag,
 )
@@ -343,29 +420,45 @@ dedup_assets_pub_task = build_bq_insert_job(
 # Set Task dependencies
 
 (
-    time_task >> write_op_stats >> op_export_task >> del_ins_ops_task >> delete_enrich_op_pub_task >> insert_enriched_hist_pub_task
+    time_task
+    >> write_op_stats
+    >> op_export_task
+    >> del_ins_ops_task
+    >> delete_enrich_op_pub_task
+    >> insert_enriched_hist_pub_task
+)
+
+(time_task >> write_trade_stats >> trade_export_task >> del_ins_trades_task)
+
+(time_task >> write_effects_stats >> effects_export_task >> del_ins_effects_task)
+
+(
+    time_task
+    >> write_tx_stats
+    >> tx_export_task
+    >> del_ins_txns_task
+    >> delete_enrich_op_pub_task
 )
 
 (
-    time_task >> write_trade_stats >> trade_export_task >> del_ins_trades_task
+    time_task
+    >> write_ledger_stats
+    >> ledger_export_task
+    >> del_ins_ledgers_task
+    >> delete_enrich_op_pub_task
 )
 
 (
-    time_task >> write_effects_stats >> effects_export_task >> del_ins_effects_task
+    time_task
+    >> write_asset_stats
+    >> asset_export_task
+    >> del_ins_assets_task
+    >> dedup_assets_pub_task
 )
 
 (
-    time_task >> write_tx_stats >> tx_export_task >> del_ins_txns_task >> delete_enrich_op_pub_task
-)
-
-(
-    time_task >> write_ledger_stats >> ledger_export_task >> del_ins_ledgers_task >> delete_enrich_op_pub_task
-)
-
-(
-    time_task >> write_asset_stats >> asset_export_task >> del_ins_assets_task >> dedup_assets_pub_task
-)
-
-(
-    time_task >> write_contract_events_stats >> contract_events_export_task >> del_ins_cont_events_task
+    time_task
+    >> write_contract_events_stats
+    >> contract_events_export_task
+    >> del_ins_cont_events_task
 )
