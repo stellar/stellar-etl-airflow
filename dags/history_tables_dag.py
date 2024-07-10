cc441554d8c0e45d4ecb71d17b4ffe967a605483
harsha-stellar-data <sriharsha.pasupuleti@stellar.org>
2024-07-10 17:13:46 -0600

    Updated the code to use Kwargs, Added Comments

--- dags/history_tables_dag.py
+++ dags/history_tables_dag.py
@@ -57,7 +57,7 @@ dag = DAG(
         "subtract_data_interval": macros.subtract_data_interval,
         "batch_run_date_as_datetime_string": macros.batch_run_date_as_datetime_string,
     },
-    sla_miss_callback=alert_sla_miss,
+    # sla_miss_callback=alert_sla_miss,
 )
 
 # Initialize batch metadata variables
@@ -73,7 +73,35 @@ use_futurenet = literal_eval(Variable.get("use_futurenet"))
 use_captive_core = literal_eval(Variable.get("use_captive_core"))
 txmeta_datastore_path = "{{ var.value.txmeta_datastore_path }}"
 
-# Define export tasks
+"""
+The time task reads in the execution time of the current run, as well as the next
+execution time. It converts these two times into ledger ranges.
+"""
+time_task = build_time_task(dag, use_testnet=use_testnet, use_futurenet=use_futurenet)
+
+"""
+The build batch stats task will take a snapshot of the DAG run_id, execution date,
+start and end ledgers so that reconciliation and data validation are easier. The
+record is written to an internal dataset for data eng use only.
+"""
+write_op_stats = build_batch_stats(dag, table_names["operations"])
+write_trade_stats = build_batch_stats(dag, table_names["trades"])
+write_effects_stats = build_batch_stats(dag, table_names["effects"])
+write_tx_stats = build_batch_stats(dag, table_names["transactions"])
+write_ledger_stats = build_batch_stats(dag, table_names["ledgers"])
+write_asset_stats = build_batch_stats(dag, table_names["assets"])
+write_contract_events_stats = build_batch_stats(dag, "contract_events")
+
+"""
+The export tasks call export commands on the Stellar ETL using the ledger range from the time task.
+The results of the command are stored in a file. There is one task for each of the data types that
+can be exported from the history archives.
+
+The DAG sleeps for 30 seconds after the export_task writes to the file to give the poststart.sh
+script time to copy the file over to the correct directory. If there is no sleep, the load task
+starts prematurely and will not load data.
+"""
+
 op_export_task = build_export_task(
     dag,
     "archive",
@@ -158,7 +186,14 @@ contract_events_export_task = build_export_task(
     txmeta_datastore_path=txmeta_datastore_path,
 )
 
-# Define del_ins tasks
+"""
+The delete part of the task checks to see if the given partition/batch id exists in
+Bigquery. If it does, the records are deleted prior to reinserting the batch.
+
+The Insert part of the task receive the location of the file in Google Cloud storage through Airflow's XCOM system.
+Then, the task merges the unique entries in the file into the corresponding table in BigQuery.
+
+"""
 del_ins_tasks = {}
 export_tasks = {
     "operations": op_export_task,
@@ -184,17 +219,9 @@ for data_type, export_task in export_tasks.items():
         dag, f"del_ins_{data_type}_task", task_vars, build_del_ins_from_gcs_to_bq_task
     )
 
-# Define time task
-time_task = build_time_task(dag, use_testnet=use_testnet, use_futurenet=use_futurenet)
-
-# Define other tasks
-write_op_stats = build_batch_stats(dag, table_names["operations"])
-write_trade_stats = build_batch_stats(dag, table_names["trades"])
-write_effects_stats = build_batch_stats(dag, table_names["effects"])
-write_tx_stats = build_batch_stats(dag, table_names["transactions"])
-write_ledger_stats = build_batch_stats(dag, table_names["ledgers"])
-write_asset_stats = build_batch_stats(dag, table_names["assets"])
-write_contract_events_stats = build_batch_stats(dag, "contract_events")
+"""
+Delete and Insert operations for the Enriched History Operations table
+"""
 
 delete_enrich_op_pub_task = build_delete_data_task(
     dag,
@@ -225,7 +252,8 @@ dedup_assets_pub_task = build_bq_insert_job(
     dataset_type="pub",
 )
 
-# Set dependencies
+# Set Task dependencies
+
 (
     time_task
     >> write_op_stats
