e1c3739c20ffad2bba397d8168bd23ec65af3672
Amisha Singla <amishas157@gmail.com>
2024-09-12 15:44:52 +0530

    Clean metadates from DBT (#488)
    * export start timestamp
    
    * update dbt image
    
    * Use task instance variable
    
    * convert format
    
    * convert format
    
    * Update DBT image
    
    * update images

--- dags/stellar_etl_airflow/build_dbt_task.py
+++ dags/stellar_etl_airflow/build_dbt_task.py
@@ -133,6 +133,7 @@ def dbt_task(
             "PUBLIC_SOURCE_DB": "{{ var.value.dbt_public_source_db }}",
             "PUBLIC_SOURCE_SCHEMA": "{{ var.value.dbt_public_source_schema }}",
             "EXECUTION_DATE": "{{ ts }}",
+            "AIRFLOW_START_TIMESTAMP": "{{ ti.start_date.strftime('%Y-%m-%dT%H:%M:%SZ') }}",
         },
         image=dbt_image,
         arguments=args,
