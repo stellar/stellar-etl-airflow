58678999e30f247997682f9610d241c0e6152234
Amisha Singla <amishas157@gmail.com>
2024-09-25 15:39:22 +0530

    Update dags/dbt_recency_tests_dag.py
    Co-authored-by: chowbao <simon.chow@stellar.org>

--- dags/dbt_recency_tests_dag.py
+++ dags/dbt_recency_tests_dag.py
@@ -17,7 +17,7 @@ with DAG(
     "dbt_recency_tests",
     default_args=get_default_dag_args(),
     start_date=datetime(2024, 9, 30, 0, 0),
-    description="This DAG runs non-model dbt tests half-hourly cadence",
+    description="This DAG runs recency tests at a daily cadence",
     schedule="0 20 * * *",  # Every day at 8:00 PM UTC / 3:00 PM CST
     user_defined_filters={
         "container_resources": lambda s: k8s.V1ResourceRequirements(requests=s),
