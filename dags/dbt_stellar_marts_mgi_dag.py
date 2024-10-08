46a29c14fb01278a9010c83c62a012400e093940
Amisha Singla <amishas157@gmail.com>
2024-10-08 16:53:23 -0400

    Update the schedule for MGI Dag (#508)

--- dags/dbt_stellar_marts_mgi_dag.py
+++ dags/dbt_stellar_marts_mgi_dag.py
@@ -17,7 +17,7 @@ dag = DAG(
     default_args=get_default_dag_args(),
     start_date=datetime(2024, 10, 1, 0, 0),
     description="This DAG runs dbt models at a daily cadence",
-    schedule_interval="0 17 * * *",  # Runs at 05:00 PM UTC / 11:00 AM CST as MGI files are received around ~10:00 AM CST
+    schedule_interval="0 16 * * *",  # Runs at 04:00 PM UTC / 10:00 AM CST as MGI files are received around ~10:00 AM CST
     user_defined_filters={
         "container_resources": lambda s: k8s.V1ResourceRequirements(requests=s),
     },
