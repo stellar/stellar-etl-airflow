9359a4ca3ec04a015fc61762d8790b03d42d61f2
Amisha Singla <amishas157@gmail.com>
2024-10-29 15:00:37 -0500

    use the actual table

--- scripts/update_data_schema_changelog.sh
+++ scripts/update_data_schema_changelog.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# result=$(bq query --format=prettyjson --nouse_legacy_sql \
-# 'SELECT
-#         date(detected_at) as date
-#         , database_name
-#         , schema_name
-#         , table_name
-#         , sub_type
-#         , ARRAY_AGG(column_name) as columns
-#  FROM
-#    `hubble-261722`.elementary.alerts_schema_changes
-#     GROUP BY
-#     1, 2, 3, 4, 5
-#     ORDER BY 1 DESC, 2 DESC, 3 DESC, 4 ASC
-# ')
+result=$(bq query --nouse_legacy_sql \
+'SELECT
+        date(detected_at) as date
+        , database_name
+        , schema_name
+        , table_name
+        , sub_type
+        , ARRAY_AGG(column_name) as columns
+ FROM
+   `hubble-261722`.elementary.alerts_schema_changes
+    GROUP BY
+    1, 2, 3, 4, 5
+    ORDER BY 1 DESC, 2 DESC, 3 DESC, 4 ASC
+')
 
-result=$(bq query --nouse_legacy_sql 'SELECT count(*) from `hubble-261722`.crypto_stellar_internal_2.country_code')
+# result=$(bq query --nouse_legacy_sql 'SELECT count(*) from `hubble-261722`.crypto_stellar_internal_2.country_code')
 echo "# Changes in data schema"
 echo "$result"
 
