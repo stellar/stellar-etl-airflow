321716ab1f0b5365edc795499c0e701696b79cdf
Amisha Singla <amishas157@gmail.com>
2024-11-11 19:33:12 -0600

    Actually use ephemeral storage

--- dags/stellar_etl_airflow/build_elementary_slack_alert_task.py
+++ dags/stellar_etl_airflow/build_elementary_slack_alert_task.py
@@ -35,6 +35,7 @@ def elementary_task(
         requests={
             "cpu": f"{{{{ var.json.resources.{resource_cfg}.requests.cpu }}}}",
             "memory": f"{{{{ var.json.resources.{resource_cfg}.requests.memory }}}}",
+            "ephemeral-storage": f"{{{{ var.json.resources.{resource_cfg}.requests.ephemeral-storage }}}}",
         }
     )
 
