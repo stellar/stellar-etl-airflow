8f340b1e317ef12c5dfd4d01d3f46a68b61b5e19
Amisha Singla <amishas157@gmail.com>
2025-02-24 13:02:06 -0600

    update task sla

--- dags/dbt_stellar_marts_dag.py
+++ dags/dbt_stellar_marts_dag.py
@@ -85,6 +85,7 @@ wait_on_dbt_enriched_base_tables >> asset_stats_agg_task
 wait_on_dbt_enriched_base_tables >> network_stats_agg_task
 wait_on_dbt_enriched_base_tables >> partnership_assets_task
 wait_on_dbt_enriched_base_tables >> history_assets
+wait_on_dbt_enriched_base_tables >> wallet_metrics_task
 # wait_on_dbt_enriched_base_tables >> soroban
 # wait_on_dbt_enriched_base_tables >> snapshot_state
 # wait_on_dbt_enriched_base_tables >> relevant_asset_trades
