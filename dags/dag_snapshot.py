a294bec59698bdb3306a0580b745795afad315ae
Amisha Singla <amishas157@gmail.com>
2025-04-07 21:53:16 +0530

    Add task for accounts snapshot

--- dags/dag_snapshot.py
+++ dags/dag_snapshot.py
@@ -38,11 +38,23 @@ dag = DAG(
 
 trustline_snapshot_task = dbt_task(
     dag,
-    tag="custom_snapshot",
+    tag="custom_snapshot_trustline",
     excluded="stellar_dbt_public",
     env_vars={
         "BACKFILL_START_DATE": "{{ ds if run_id.startswith('scheduled_') else params.backfill_start_date }}",
         "BACKFILL_END_DATE": "{{ macros.ds_add(ds, 1) if run_id.startswith('scheduled_') else params.backfill_end_date }}",
     },
 )
+
+accounts_snapshot_task = dbt_task(
+    dag,
+    tag="custom_snapshot_accounts",
+    excluded="stellar_dbt_public",
+    env_vars={
+        "BACKFILL_START_DATE": "{{ ds if run_id.startswith('scheduled_') else params.backfill_start_date }}",
+        "BACKFILL_END_DATE": "{{ macros.ds_add(ds, 1) if run_id.startswith('scheduled_') else params.backfill_end_date }}",
+    },
+)
+
 trustline_snapshot_task
+accounts_snapshot_task
