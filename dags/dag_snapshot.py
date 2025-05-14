2e46ab6eb68d53c0b02ed13604f904d3e40be552
Amisha Singla <amishas157@gmail.com>
2025-05-14 12:51:16 -0500

    Add option to skip snapshot task (#588)
    * remove timedelta
    
    * update start date
    
    * Revert "remove timedelta"
    
    This reverts commit e9d08c9b02c874717367e20ce2631479e99425b5.
    
    * add short circuit operator
    
    * lint
    
    * Add task to snapshot liquidity pools
    
    lint
    
    * Update airflow variables task sla
    
    lint
    
    * Update dags/dag_snapshot.py
    
    Co-authored-by: chowbao <simon.chow@stellar.org>
    
    * Revert "Update dags/dag_snapshot.py"
    
    This reverts commit 0fb8f6236dc551679e556a1f162a7d45c0856686.
    
    ---------
    
    Co-authored-by: chowbao <simon.chow@stellar.org>

--- dags/dag_snapshot.py
+++ dags/dag_snapshot.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.models.param import Param
+from airflow.operators.python import ShortCircuitOperator
 from kubernetes.client import models as k8s
 from stellar_etl_airflow.build_cross_dependency_task import build_cross_deps
 from stellar_etl_airflow.build_dbt_task import dbt_task
@@ -35,12 +36,61 @@ dag = DAG(
         "snapshot_full_refresh": Param(
             default="false", type="string"
         ),  # only used for manual runs
+        "skip_trustline": Param(
+            default="false", type="string"
+        ),  # only used for manual runs
+        "skip_accounts": Param(
+            default="false", type="string"
+        ),  # only used for manual runs
+        "skip_claimable_balance": Param(
+            default="false", type="string"
+        ),  # only used for manual runs
+        "skip_liquidity_pools": Param(
+            default="false", type="string"
+        ),  # only used for manual runs
     },
     # sla_miss_callback=alert_sla_miss,
 )
 
+
+def should_run_task(snapshot_name, **kwargs):
+    return kwargs["params"].get(snapshot_name, "false") != "true"
+
+
 wait_on_dbt_enriched_base_tables = build_cross_deps(
     dag, "wait_on_dbt_enriched_base_tables", "dbt_enriched_base_tables", time_delta=90
+)
+
+check_should_run_trustline = ShortCircuitOperator(
+    task_id="check_should_run_trustline",
+    python_callable=should_run_task,
+    op_args=["skip_trustline"],
+    provide_context=True,
+    dag=dag,
+)
+
+check_should_run_claimable_balance = ShortCircuitOperator(
+    task_id="check_should_run_claimable_balance",
+    python_callable=should_run_task,
+    op_args=["skip_claimable_balance"],
+    provide_context=True,
+    dag=dag,
+)
+
+check_should_run_accounts = ShortCircuitOperator(
+    task_id="check_should_run_accounts",
+    python_callable=should_run_task,
+    op_args=["skip_accounts"],
+    provide_context=True,
+    dag=dag,
+)
+
+check_should_run_liquidity_pools = ShortCircuitOperator(
+    task_id="check_should_run_liquidity_pools",
+    python_callable=should_run_task,
+    op_args=["skip_liquidity_pools"],
+    provide_context=True,
+    dag=dag,
 )
 
 trustline_snapshot_task = dbt_task(
@@ -76,6 +126,30 @@ claimable_balances_snapshot_task = dbt_task(
     },
 )
 
-wait_on_dbt_enriched_base_tables >> trustline_snapshot_task
-wait_on_dbt_enriched_base_tables >> accounts_snapshot_task
-wait_on_dbt_enriched_base_tables >> claimable_balances_snapshot_task
+liquidity_pools_snapshot_task = dbt_task(
+    dag,
+    tag="custom_snapshot_liquidity_pools",
+    excluded="stellar_dbt_public",
+    env_vars={
+        "SNAPSHOT_START_DATE": "{{ ds if run_id.startswith('scheduled_') else params.snapshot_start_date }}",
+        "SNAPSHOT_END_DATE": "{{ next_ds if run_id.startswith('scheduled_') else params.snapshot_end_date }}",
+        "SNAPSHOT_FULL_REFRESH": "{{ false if run_id.startswith('scheduled_') else params.snapshot_full_refresh }}",
+    },
+)
+
+(
+    wait_on_dbt_enriched_base_tables
+    >> check_should_run_trustline
+    >> trustline_snapshot_task
+)
+wait_on_dbt_enriched_base_tables >> check_should_run_accounts >> accounts_snapshot_task
+(
+    wait_on_dbt_enriched_base_tables
+    >> check_should_run_claimable_balance
+    >> claimable_balances_snapshot_task
+)
+(
+    wait_on_dbt_enriched_base_tables
+    >> check_should_run_liquidity_pools
+    >> liquidity_pools_snapshot_task
+)
