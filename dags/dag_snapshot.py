e5c74e39bcc5271f2b3c43dddf38ee8465f9b4ed
Amisha Singla <amishas157@gmail.com>
2025-09-12 12:18:18 -0500

    Update dbt image and do not exclude stellar dbt public

--- dags/dag_snapshot.py
+++ dags/dag_snapshot.py
@@ -108,7 +108,6 @@ trustline_snapshot_task = dbt_task(
 accounts_snapshot_task = dbt_task(
     dag,
     tag="custom_snapshot_accounts",
-    excluded="stellar_dbt_public",
     env_vars={
         "SNAPSHOT_START_DATE": "{{ ds if run_id.startswith('scheduled_') else params.snapshot_start_date }}",
         "SNAPSHOT_END_DATE": "{{ next_ds if run_id.startswith('scheduled_') else params.snapshot_end_date }}",
