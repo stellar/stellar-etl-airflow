523ef85a5ba3745976c5123ba0463224835ac549
Amisha Singla <amishas157@gmail.com>
2025-09-26 00:18:24 -0500

    Add task for wisdom prices

--- dags/external_data_dag.py
+++ dags/external_data_dag.py
@@ -23,14 +23,18 @@ init_sentry()
 EXTERNAL_DATA_TABLE_NAMES = Variable.get("table_ids", deserialize_json=True)
 EXTERNAL_DATA_PROJECT_NAME = Variable.get("bq_project")
 EXTERNAL_DATA_DATASET_NAME = Variable.get("bq_dataset")
+
 RETOOL_TABLE_NAME = EXTERNAL_DATA_TABLE_NAMES["retool_entity_data"]
 RETOOL_EXPORT_TASK_ID = "export_retool_data"
+
+WISDOM_PRICES_TABLE_NAME = EXTERNAL_DATA_TABLE_NAMES["wisdom_prices_data"]
+WISDOM_PRICES_EXPORT_TASK_ID = "export_wisdom_prices_data"
 
 # Initialize the DAG
 dag = DAG(
     "external_data_dag",
     default_args=get_default_dag_args(),
-    start_date=datetime(2024, 12, 16, 0, 0),
+    start_date=datetime(2025, 1, 1, 0, 0),
     description="This DAG exports data from external sources such as retool.",
     schedule_interval="0 22 * * *",
     params={
@@ -46,6 +50,7 @@ dag = DAG(
         "container_resources": lambda s: k8s.V1ResourceRequirements(requests=s),
         "literal_eval": lambda e: literal_eval(e),
     },
+    catchup=True,
 )
 
 
@@ -79,3 +84,35 @@ retool_insert_to_bq_task = create_export_del_insert_operator(
 )
 
 retool_export_task >> retool_insert_to_bq_task
+
+
+wisdom_prices_export_task = build_export_task(
+    dag,
+    WISDOM_PRICES_EXPORT_TASK_ID,
+    command="export-wisdom-prices",
+    cmd_args=[
+        "--start-time",
+        "{{ subtract_data_interval(dag, data_interval_start).isoformat() }}",
+        "--end-time",
+        "{{ subtract_data_interval(dag, data_interval_end).isoformat() }}",
+    ],
+    use_gcs=True,
+    env_vars={
+        "RWA_API_KEY": access_secret("rwa-api-key", "default"),
+    },
+)
+
+
+wisdom_prices_insert_to_bq_task = create_export_del_insert_operator(
+    dag,
+    table_name=WISDOM_PRICES_TABLE_NAME,
+    project=EXTERNAL_DATA_PROJECT_NAME,
+    dataset=EXTERNAL_DATA_DATASET_NAME,
+    export_task_id=WISDOM_PRICES_EXPORT_TASK_ID,
+    source_object_suffix="",
+    partition=False,
+    cluster=False,
+    table_id=f"{EXTERNAL_DATA_PROJECT_NAME}.{EXTERNAL_DATA_DATASET_NAME}.{WISDOM_PRICES_TABLE_NAME}",
+)
+
+wisdom_prices_export_task >> wisdom_prices_insert_to_bq_task
