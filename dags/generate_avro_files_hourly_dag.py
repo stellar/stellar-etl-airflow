1ca815c0e4b050f9010653a1e62acb05911ca94b
Simon Chow <simon.chow@stellar.org>
2026-01-13 15:02:35 -0500

    try fix tests again

--- dags/generate_avro_files_hourly_dag.py
+++ dags/generate_avro_files_hourly_dag.py
@@ -18,7 +18,7 @@ init_sentry()
 dag = DAG(
     "generate_avro_hourly",
     default_args=get_default_dag_args(),
-    start_date=datetime(2024, 12, 17, 0, 0),
+    start_date=datetime(2026, 1, 12, 0, 0),
     catchup=True,
     description="This DAG generates AVRO files from BQ tables",
     schedule_interval="0 * * * *",
