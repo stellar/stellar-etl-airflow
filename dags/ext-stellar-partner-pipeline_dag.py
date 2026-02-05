3b27c4b894c2076535c6f7eafca3f3e7ef85d068
Trevor Thayer <trevor.thayer32@gmail.com>
2026-02-05 14:40:02 -0800

    [568] Created DAG to bring in external tables to BQ from ext-stellar-partner-data bucket

---
+++ dags/ext-stellar-partner-pipeline_dag.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.providers.google.cloud.sensors.gcs import (
+    GCSObjectsWithPrefixExistenceSensor,
+)
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
+    GCSToBigQueryOperator,
+)
+from stellar_etl_airflow.build_apply_gcs_changes_to_bq_task import read_local_schema
+from stellar_etl_airflow.default import (
+    alert_after_max_retries,
+    alert_sla_miss,
+    get_default_dag_args,
+    init_sentry,
+)
+
+init_sentry()
+
+with DAG(
+    "ext-stellar-partner-pipeline_dag",
+    default_args=get_default_dag_args(),
+    start_date=datetime(2023, 1, 1, 0, 0),
+    description="This DAG automates monthly updates from partner address dumps in GCS to partner stg_addresses_____ tables in BigQuery.",
+    schedule_interval='@monthly',
+    params={
+        "alias": "partner-addresses",
+    },
+    render_template_as_native_obj=True,
+    catchup=False,
+    sla_miss_callback=alert_sla_miss,
+) as dag:
+    PROJECT = "{{ var.value.bq_project }}"
+    DATASET = "{{ var.value.bq_dataset }}"
+    BUCKET_NAME = "{{ var.value.partner_addresses_bucket }}"
+    PARTNERS = Variable.get("partner_addresses_data", deserialize_json=True)
+    start_tables_task = EmptyOperator(task_id="start_update_task")
+
+    for partner in PARTNERS:
+        OBJECT_PREFIX = "{}/{}_".format(
+            PARTNERS[partner]["prefix_folder"], PARTNERS[partner]["prefix_id"]
+        )
+        check_gcs_file = GCSObjectsWithPrefixExistenceSensor(
+            task_id=f"check_gcs_file_{partner}_addresses",
+            bucket=BUCKET_NAME,
+            prefix=OBJECT_PREFIX,
+            dag=dag,
+            poke_interval=60,
+            timeout=3600,
+            on_failure_callback=alert_after_max_retries,
+        )
+
+        send_partner_addresses_to_bq_internal_task = GCSToBigQueryOperator(
+            task_id=f"send_{partner}_addresses_to_bq_pub_task",
+            bucket=BUCKET_NAME,
+            # This logic pulls the latest file found by the sensor (ordered alphabetically, so latest dates will be at the end)
+            source_objects=['{{ ti.xcom_pull(task_ids="check_gcs_file_' + partner + '_addresses")[-1] }}'],
+            destination_project_dataset_table="{}.{}.{}".format(
+                PROJECT, DATASET, PARTNERS[partner]["table"]
+            ),
+            skip_leading_rows=1,
+            schema_fields=read_local_schema(PARTNERS[partner]["table"]),
+            write_disposition="WRITE_TRUNCATE",
+            dag=dag,
+            retry_delay=timedelta(minutes=5),
+            max_retry_delay=timedelta(minutes=30),
+            on_failure_callback=alert_after_max_retries,
+        )
+
+        (start_tables_task >> check_gcs_file >> send_partner_addresses_to_bq_internal_task)
