38bf6cccf4b846a8dee2d6a574bc4f579c0e5539
Trevor Thayer <trevor.thayer32@gmail.com>
2026-02-12 14:32:50 -0800

    updates to airflow vars for staging and test

--- dags/ext_stellar_partner_pipeline_dag.py
+++ dags/ext_stellar_partner_pipeline_dag.py
@@ -42,14 +42,14 @@ init_sentry()
 with DAG(
     "ext_stellar_partner_pipeline_dag",
     default_args=get_default_dag_args(),
-    start_date=datetime(2023, 1, 1, 0, 0),
+    start_date=datetime(2026, 2, 1, 0, 0),
     description="This DAG automates monthly updates from partner data dumps in GCS to partner tables in BigQuery.",
     schedule_interval="0 0 1 * *",
     params={
         "alias": "partner-data",
     },
     render_template_as_native_obj=True,
-    catchup=False,
+    catchup=True,
     sla_miss_callback=alert_sla_miss,
 ) as dag:
     PROJECT = "{{ var.value.bq_project }}"
